87f26ad12510c5a3cc075e07e15b269a97ab8baf
KP64 <karamalsadeh@hotmail.com>
2024-05-08 13:05:17 +0200

    Initial Commit

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    inputs@{
+      self,
+      nixpkgs,
+      home-manager,
+      ...
+    }:
+    {
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = { inherit inputs; };
+        modules = [
+          ./configuration.nix
+
+          home-manager.nixosModules.default
+          {
+            home-manager = {
+              extraSpecialArgs = { inherit inputs; };
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              users.kg = import ./home.nix;
+            };
+          }
+        ];
+      };
+    };
+}
---
+++ home.nix
@@ -0,0 +1,152 @@
+{ 
+  # config, 
+  pkgs,
+  # inputs, 
+  ... 
+}:
+
+{
+  nix.gc.automatic = true;
+
+  home = {
+    stateVersion = "24.05";
+    username = "kg";
+    homeDirectory = "/home/kg";
+    packages = with pkgs; [
+      asciinema
+      ani-cli
+      tokei
+      sshx
+      just
+      lychee
+      gping
+      hexyl
+
+      onefetch
+      hyperfine
+      hurl
+      gitoxide
+
+      dust
+      procs
+      sd
+      xcp
+      jnv
+      glow
+      kondo
+    ];
+  };
+
+  services.pueue.enable = true;
+
+  wayland.windowManager.hyprland = {
+    enable = true;
+    extraConfig = builtins.readFile ./hyprland.conf;
+  };
+
+  programs = {
+    home-manager.enable = true;
+
+    waybar = {
+      enable = true;
+      systemd.enable = true;
+    };
+    
+    bottom.enable = true;
+    
+    thefuck.enable = true;
+    
+    fzf.enable = true;
+    
+    direnv = {
+      enable = true;
+      nix-direnv.enable = true;
+    };
+    
+    tealdeer.enable = true;
+    
+    nushell = {
+      enable = true;
+      configFile.text= ''
+      $env.config = {
+        show_banner: false
+      }
+      '';
+    };
+    
+    zellij = {
+      enable = true;
+      settings = { };
+    };
+    
+    bat.enable = true;
+    
+    lsd = {
+      enable = true;
+      enableAliases = true;
+    };
+    
+    navi.enable = true;
+    
+    ripgrep.enable = true;
+    
+    fd.enable = true;
+    
+    zoxide = {
+      enable = true;
+      options = [ "--cmd cd" ];
+    };
+
+    xplr.enable = true;
+    
+    gitui.enable = true;
+    
+    git = {
+      enable = true;
+      userName = "KP64";
+      userEmail = "karamalsadeh@hotmail.com";
+      delta.enable = true;
+      extraConfig = {
+        init.defaultBranch = "master";
+        safe.directory = "/etc/nixos";
+      };
+    };
+
+    atuin = {
+      enable = true;
+      settings = {
+        invert = true;
+        filter_mode_shell_up_key_binding = "directory";
+      };
+    };
+
+    bash = {
+      enable = true;
+      initExtra = "nu";
+    };
+    
+    kitty = {
+      enable = true;
+      font.name = "JetBrainsMono Nerd Font";
+    };
+    
+    starship.enable = true;
+
+    helix = {
+      enable = true;
+      defaultEditor = true;
+      settings = {
+        theme = "catppuccin_mocha";
+        editor = {
+          cursor-shape = {
+            insert = "bar";
+            normal = "block";
+            select = "underline";
+          };
+        };
+      };
+    };
+    
+    firefox.enable = true;
+  };
+}
