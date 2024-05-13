b6e4dffddd1ac458d9b81ed67ab2f1d3a8268034
KP64 <karamalsadeh@hotmail.com>
2024-05-13 22:43:21 +0200

    Modularize Text Editors

---
+++ editors/helix.nix
@@ -0,0 +1,18 @@
+_:
+
+{
+  programs.helix = {
+      enable = true;
+      defaultEditor = true;
+      settings = {
+        theme = "catppuccin_mocha";
+        editor = {
+          cursor-shape = {
+            insert = "bar";
+            normal = "block";
+            select = "underline";
+          };
+        };
+      };
+    };
+}
--- home.nix
+++ home.nix
@@ -12,6 +12,8 @@
     ./hypr/hyprland.nix
     ./hypr/hypridle.nix
     ./hypr/hyprlock.nix
+    ./editors/helix.nix
+    ./editors/vscodium.nix
   ];
 
   xdg.portal = {
@@ -243,21 +245,7 @@
 
     starship.enable = true;
 
-    helix = {
-      enable = true;
-      defaultEditor = true;
-      settings = {
-        theme = "catppuccin_mocha";
-        editor = {
-          cursor-shape = {
-            insert = "bar";
-            normal = "block";
-            select = "underline";
-          };
-        };
-      };
-    };
-
+    
     firefox = {
       enable = true;
       profiles.kg = {
@@ -283,92 +271,5 @@
       };
     };
 
-    vscode = {
-      enable = true;
-      package = pkgs.vscodium;
-      extensions =
-        with pkgs.vscode-extensions;
-        [
-          wix.vscode-import-cost
-          waderyan.gitblame
-          vscode-icons-team.vscode-icons
-          visualstudioexptteam.vscodeintellicode
-          visualstudioexptteam.intellicode-api-usage-examples
-          vadimcn.vscode-lldb
-          usernamehw.errorlens
-          thenuprojectcontributors.vscode-nushell-lang
-          tamasfe.even-better-toml
-          svelte.svelte-vscode
-          ms-azuretools.vscode-docker
-          mkhl.direnv
-          esbenp.prettier-vscode
-          eamodio.gitlens
-          catppuccin.catppuccin-vsc
-          bradlc.vscode-tailwindcss
-          bbenoist.nix
-          aaron-bond.better-comments
-        ]
-        ++ (with inputs.nix-vscode-extensions.extensions.${pkgs.system}.vscode-marketplace; [
-          rust-lang.rust-analyzer
-          miguelsolorio.fluent-icons
-          jscearcy.rust-doc-viewer
-          serayuzgur.crates
-        ]);
-      languageSnippets = {
-        toml = {
-          lint = {
-            body = [
-              "[lints.rust]"
-              ''unsafe-code = "forbid"''
-              ""
-              "[lints.clippy]"
-              ''pedantic = "warn"''
-              ''nursery = "warn"''
-              ''cargo = "warn"''
-            ];
-            description = "Lint your Rust Project to the Max";
-            prefix = [ "lint" ];
-          };
-        };
       };
-      # VScode uses US Layout (z and y are swapped)
-      # Remove keybindings when using US Layout Keyboards
-      keybindings = [
-        {
-          key = "ctrl+shift+z";
-          command = "redo";
-        }
-        {
-          key = "crtl+z";
-          command = "undo";
-        }
-        {
-          key = "ctrl+y";
-          command = "redo";
-        }
-        {
-          key = "ctrl+y";
-          command = "workbench.debug.action.toggleRepl";
-          when = "workbench.panel.repl.view.active";
-        }
-      ];
-
-      userSettings = {
-        "workbench.iconTheme" = "vscode-icons";
-        "workbench.productIconTheme" = "fluent-icons";
-        "workbench.startupEditor" = "none";
-        "files.autoSave" = "afterDelay";
-        "editor.fontFamily" = "JetBrainsMono Nerd Font";
-        "workbench.colorTheme" = "Catppuccin Mocha";
-        "editor.bracketPairColorization.independentColorPoolPerBracketType" = true;
-        "editor.guides.bracketPairs" = "active";
-        "editor.formatOnSave" = true;
-        "editor.minimap.autohide" = true;
-        "files.autoSaveWhenNoErrors" = true;
-        "files.trimTrailingWhitespace" = true;
-        "rust-analyzer.check.command" = "clippy";
-        "git.autofetch" = true;
-      };
-    };
-  };
 }
