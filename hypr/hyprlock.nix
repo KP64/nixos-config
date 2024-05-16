056014e866e308acd6489cf97cd642eafec05491
KP64 <karamalsadeh@hotmail.com>
2024-05-16 23:41:59 +0200

    Fix hyprlock Background

--- hypr/hyprlock.nix
+++ hypr/hyprlock.nix
@@ -1,30 +1,93 @@
 { inputs, pkgs, ... }:
 
 # Catppuccin doesn't support catppuccin option for hyprlock yet.
-# TODO: Replace manual fetching to options
+# TODO: Replace downloaded files with catppuccin options
 # FIXME: Catppuccin color may not match flavour. Fixed by above todo or workaround
-let
-  catlock_repo = pkgs.fetchFromGitHub {
-    owner = "catppuccin";
-    repo = "hyprlock";
-    rev = "d5a6767000409334be8413f19bfd1cf5b6bb5cc6";
-    sha256 = "sha256-pjMFPaonq3h3e9fvifCneZ8oxxb1sufFQd7hsFe6/i4=";
-  };
-  catland_repo = pkgs.fetchFromGitHub {
-    owner = "catppuccin";
-    repo = "hyprland";
-    rev = "b57375545f5da1f7790341905d1049b1873a8bb3";
-    sha256 = "sha256-XTqpmucOeHUgSpXQ0XzbggBFW+ZloRD/3mFhI+Tq4O8=";
-  };
-in
 {
-  # TODO: Try not to fetch to use local files for Backgrounds etc.
-  home.file = {
-    ".config/hypr/hyprlock.conf".source = catlock_repo + "/hyprlock.conf";
-    ".config/hypr/mocha.conf".source = catland_repo + "/themes/mocha.conf";
-  };
   programs.hyprlock = {
     enable = true;
     package = inputs.hyprlock.packages.${pkgs.system}.hyprlock;
+
+    settings = {
+      source = builtins.toString ./mocha.conf;
+
+      "$accent" = "$mauve";
+      "$accentAlpha" = "$mauveAlpha";
+      "$font" = "JetBrainsMono Nerd Font";
+      
+      general = {
+        disable_loading_bar = true;
+        hide_cursor = true;
+      };
+
+      background = [
+        {
+          monitor = "";
+          path = builtins.toString ../wallpapers/nixos-wallpaper-catppuccin-mocha.png;
+          blur_passes = 0;
+          color = "$base";
+        }
+      ];
+
+      label = [
+        {
+          monitor = "";
+          text = ''cmd[update:30000] echo "$(date +"%R")"'';
+          color = "$text";
+          font_size = 90;
+          font_family = "$font";
+          position = "-30, 0";
+          halign = "right";
+          valign = "top";
+        }
+        {
+          monitor = "";
+          text = ''cmd[update:43200000] echo "$(date +"%A, %d %B %Y")"'';
+          color = "$text";
+          font_size = 25;
+          font_family = "$font";
+          position = "-30, -150";
+          halign = "right";
+          valign = "top";
+        }
+      ];
+
+      image = [
+        {
+          monitor = "";
+          path = "~/.face"; # TODO: Add IMG
+          size = 100;
+          border_color = "$accent";
+
+          position = "0, 75";
+          halign = "center";
+          valign = "center";
+        }
+      ];
+
+      input-field = [
+        {
+          monitor = "";
+          size = "300, 60";
+          outline_thickness = 4;
+          dots_size = 0.2;
+          dots_spacing = 0.2;
+          dots_center = true;
+          outer_color = "$accent";
+          inner_color = "$surface0";
+          font_color = "$text";
+          fade_on_empty = false;
+          placeholder_text = ''<span foreground="##$textAlpha"><i>󰌾 Logged in as </i><span foreground="##$accentAlpha">$USER</span></span>'';
+          hide_input = false;
+          check_color = "$accent";
+          fail_color = "$red";
+          fail_text = ''<i>$FAIL <b>($ATTEMPTS)</b></i>'';
+          capslock_color = "$yellow";
+          position = "0, -35";
+          halign = "center";
+          valign = "center";
+        }
+      ];
+    };
   };
 }
