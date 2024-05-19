e426d609b83b3a0e0cba30b9188ba46037f58dc7
KP64 <karamalsadeh@hotmail.com>
2024-05-19 12:33:11 +0200

    Remove tuigreet

--- configuration.nix
+++ configuration.nix
@@ -119,15 +119,6 @@
       };
       videoDrivers = [ "nvidia" ];
     };
-    greetd = {
-      enable = true;
-      settings = {
-        default_session = {
-          command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd hyprland";
-          user = "kg";
-        };
-      };
-    };
   };
 
   time.timeZone = "Europe/Berlin";
