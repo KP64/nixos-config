671142cfe6e33b88768b546cb496363921cb2d6a
KP64 <karamalsadeh@hotmail.com>
2024-06-30 05:04:16 +0200

    Refactor: Styling

--- flake.nix
+++ flake.nix
@@ -169,41 +169,41 @@
   outputs =
     inputs@{ nixpkgs, ... }:
 
-    let
-      username = "kg";
-      stateVersion = "24.05";
-    in
     {
-      nixosConfigurations.${username} = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = {
-          inherit inputs username stateVersion;
-        };
-        modules =
-          with inputs;
-          [ home-manager.nixosModules.default ]
-          ++ [
-            ./hosts/${username}/configuration.nix
-            ./desktop
-            ./hardware
-            ./programs
-            ./system
+      nixosConfigurations = {
+        kg =
+          let
+            username = "kg";
+            stateVersion = "24.05";
+          in
+          nixpkgs.lib.nixosSystem {
+            system = "x86_64-linux";
+            specialArgs = {
+              inherit inputs username stateVersion;
+            };
+            modules =
+              with inputs;
+              [ home-manager.nixosModules.default ]
+              ++ [
+                ./hosts/${username}/configuration.nix
+                ./desktop
+                ./hardware
+                ./programs
+                ./system
 
-            { nixpkgs.overlays = [ inputs.nur.overlay ]; }
-
-            {
-              home-manager = {
-                extraSpecialArgs = {
-                  inherit inputs username stateVersion;
-                };
-                useGlobalPkgs = true;
-                useUserPackages = true;
-                users.${username} = {
-                  imports = [ ./hosts/${username}/home.nix ];
-                };
-              };
-            }
-          ];
+                {
+                  nixpkgs.overlays = with inputs; [ nur.overlay ];
+                  home-manager = {
+                    extraSpecialArgs = {
+                      inherit inputs username stateVersion;
+                    };
+                    useGlobalPkgs = true;
+                    useUserPackages = true;
+                    users.${username}.imports = [ ./hosts/${username}/home.nix ];
+                  };
+                }
+              ];
+          };
       };
     };
 }
