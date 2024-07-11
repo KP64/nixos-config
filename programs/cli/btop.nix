f31217ab518492380327629c8970abfbd2372c3d
KP64 <karamalsadeh@hotmail.com>
2024-07-11 20:30:44 +0200

    Fix: Btop not showing GPU

--- programs/cli/btop.nix
+++ programs/cli/btop.nix
@@ -1,4 +1,7 @@
-{ username, ... }:
+{ pkgs, username, ... }:
 {
-  home-manager.users.${username}.programs.btop.enable = true;
+  home-manager.users.${username}.programs.btop = {
+    enable = true;
+    package = pkgs.btop.override { cudaSupport = true; };
+  };
 }
