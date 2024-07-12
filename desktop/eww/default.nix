9b46357f9fcce1e8c1c119c7bad0d31f7803ce77
KP64 <karamalsadeh@hotmail.com>
2024-07-12 15:49:48 +0200

    Add: Initial Eww Config

--- desktop/eww/default.nix
+++ desktop/eww/default.nix
@@ -10,13 +10,11 @@
   options.desktop.eww.enable = lib.mkEnableOption "Enables eww";
 
   config = lib.mkIf config.desktop.eww.enable {
-    # TODO: Configs are "Borrowed". Make own Config Files ASAP.
-    home-manager.users.${username} = {
-      home.packages = with inputs; [ eww.packages.${pkgs.system}.eww ];
-      xdg.configFile = {
-        "eww/eww.yuck".source = ./eww.yuck;
-        "eww/eww.scss".source = ./eww.scss;
-      };
+    services.playerctld.enable = true;
+    home-manager.users.${username}.programs.eww = {
+      enable = true;
+      package = inputs.eww.packages.${pkgs.system}.eww;
+      configDir = ./conf;
     };
   };
 }
