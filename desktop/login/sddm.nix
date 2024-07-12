ad9768d7444e4360aa46e37ee9fe3c6f77bdd1b4
KP64 <karamalsadeh@hotmail.com>
2024-07-12 23:56:40 +0200

    Remove: Wrong TODO

--- desktop/login/sddm.nix
+++ desktop/login/sddm.nix
@@ -9,7 +9,6 @@
   options.desktop.login.sddm.enable = lib.mkEnableOption "Enables sddm";
 
   # FIXME: Only logins to Steam instead of Hyprland
-  # TODO: The cmd is wrong when hyprland is not enabled
   config = lib.mkIf config.desktop.login.sddm.enable {
     services.displayManager.sddm = {
       enable = true;
