ecfd02087e6d106cf7b2c0b2d95a5a49c7d9bd93
KP64 <karamalsadeh@hotmail.com>
2024-07-13 14:38:47 +0200

    Change: btop show gpu0 by default

--- programs/cli/btop.nix
+++ programs/cli/btop.nix
@@ -2,6 +2,9 @@
 {
   home-manager.users.${username}.programs.btop = {
     enable = true;
+    # Allows GPU detection for Nvidia cards
     package = pkgs.btop.override { cudaSupport = true; };
+    # gpu0 not shown by default
+    settings.shown_boxes = "cpu mem net proc gpu0";
   };
 }
