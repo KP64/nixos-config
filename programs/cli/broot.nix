a9c28ddecfc2ef6a38dc0a90d7471076a3178ae5
KP64 <karamalsadeh@hotmail.com>
2024-07-20 06:43:03 +0200

    Revert: broot verbs
    The custom verb was overriding the renaming invocation also done through 'F2'. It was rarely used too therefore the motivation to revert.

--- programs/cli/broot.nix
+++ programs/cli/broot.nix
@@ -1,17 +1,4 @@
 { username, ... }:
 {
-  # TODO: Change Colors to Catppuccin
-  home-manager.users.${username}.programs.broot = {
-    enable = true;
-    settings.verbs = [
-      {
-        invocation = "edit";
-        key = "F2";
-        shortcut = "e";
-        apply_to = "file";
-        external = "$EDITOR {file}";
-        leave_broot = false;
-      }
-    ];
-  };
+  home-manager.users.${username}.programs.broot.enable = true;
 }
