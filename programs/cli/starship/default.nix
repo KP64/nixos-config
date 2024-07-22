8cbf66a264f1a9352dcc84ef241d5abeda1bc1ba
KP64 <karamalsadeh@hotmail.com>
2024-07-22 15:12:16 +0200

    chore: :truck: Remove starship prefix

--- programs/cli/starship/default.nix
+++ programs/cli/starship/default.nix
@@ -2,6 +2,6 @@
 {
   home-manager.users.${username}.programs.starship = {
     enable = true;
-    settings = fromTOML (builtins.readFile ./starship_preset.toml);
+    settings = fromTOML (builtins.readFile ./preset.toml);
   };
 }
