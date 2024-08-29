159f8b6356606cd9a0613d32d1de55a49989b72e
KP64 <karamalsadeh@hotmail.com>
2024-08-30 00:21:06 +0200

    Hide more engines

--- programs/firefox/default.nix
+++ programs/firefox/default.nix
@@ -39,6 +39,8 @@ in
               hideEngines [
                 "Bing"
                 "Google"
+                "DuckDuckGo"
+                "Wikipedia (en)"
               ]
               // {
                 SearXNG = {
