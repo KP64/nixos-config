af811000e1c679aabaf60d5b8b34e735d58113f8
KP64 <karamalsadeh@hotmail.com>
2024-09-21 02:13:41 +0200

    Fix: Android No hostname

--- hosts/ad/configuration.nix
+++ hosts/ad/configuration.nix
@@ -1,4 +1,4 @@
-{ stateVersion, ... }:
+{ username, stateVersion, ... }:
 {
   editors.helix.enable = true;
 
@@ -9,7 +9,26 @@
     inherit stateVersion;
   };
 
+  networking.hostName = username;
+
   time.timeZone = "Europe/Berlin";
+
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    extraLocaleSettings = {
+      LC_ADDRESS = "de_DE.UTF-8";
+      LC_IDENTIFICATION = "de_DE.UTF-8";
+      LC_MEASUREMENT = "de_DE.UTF-8";
+      LC_MONETARY = "de_DE.UTF-8";
+      LC_NAME = "de_DE.UTF-8";
+      LC_NUMERIC = "de_DE.UTF-8";
+      LC_PAPER = "de_DE.UTF-8";
+      LC_TELEPHONE = "de_DE.UTF-8";
+      LC_TIME = "de_DE.UTF-8";
+    };
+  };
+  console.keyMap = "de";
+  nixpkgs.config.allowUnfree = true;
 
   # Configure home-manager
   home-manager.backupFileExtension = "hm-bak";
