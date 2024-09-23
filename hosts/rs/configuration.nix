c82fa75a918c7ea65db06947446d9026bf9e0b1e
KP64 <karamalsadeh@hotmail.com>
2024-09-23 19:43:20 +0200

    Add: Adguard Home

--- hosts/rs/configuration.nix
+++ hosts/rs/configuration.nix
@@ -90,8 +90,334 @@
   console.keyMap = "de";
   nixpkgs.config.allowUnfree = true;
 
+  environment.systemPackages = [ pkgs.adguardian ];
   services = {
     openssh.enable = true;
+    adguardhome = {
+      enable = true;
+      allowDHCP = false;
+      openFirewall = true;
+      mutableSettings = false;
+      settings = {
+        http = {
+          pprof = {
+            port = 6060;
+            enabled = false;
+          };
+          session_ttl = "720h";
+        };
+        users = [
+          {
+            name = "ka64";
+            password = "$2a$10$47XJ6KSFE4uXqACmYQQDaeDA4u6PVbCe8qD3xkcxel8TpwWBApawe";
+          }
+        ];
+        auth_attempts = 5;
+        block_auth_min = 15;
+        http_proxy = "";
+        language = "";
+        theme = "auto";
+        dns = {
+          bind_hosts = [ "0.0.0.0" ];
+          port = 53;
+          anonymize_client_ip = false;
+          ratelimit = 20;
+          ratelimit_subnet_len_ipv4 = 24;
+          ratelimit_subnet_len_ipv6 = 56;
+          ratelimit_whitelist = [ ];
+          refuse_any = true;
+          upstream_dns = [
+            "https://dns10.quad9.net/dns-query"
+            # EDNS & DNSSEC Support
+            "https://dns11.quad9.net/dns-query"
+          ];
+          upstream_dns_file = "";
+          bootstrap_dns = [
+            "9.9.9.10"
+            "149.112.112.10"
+            "2620:fe::10"
+            "2620:fe::fe:10"
+          ];
+          fallback_dns = [ ];
+          upstream_mode = "load_balance";
+          fastest_timeout = "1s";
+          allowed_clients = [ ];
+          disallowed_clients = [ ];
+          blocked_hosts = [
+            "version.bind"
+            "id.server"
+            "hostname.bind"
+          ];
+          trusted_proxies = [
+            "127.0.0.0/8"
+            "::1/128"
+          ];
+          cache_size = 4194304;
+          cache_ttl_min = 0;
+          cache_ttl_max = 0;
+          cache_optimistic = false;
+          bogus_nxdomain = [ ];
+          aaaa_disabled = false;
+          enable_dnssec = true;
+          edns_client_subnet = {
+            custom_ip = "";
+            enabled = false;
+            use_custom = false;
+          };
+          max_goroutines = 300;
+          handle_ddr = true;
+          ipset = [ ];
+          ipset_file = "";
+          bootstrap_prefer_ipv6 = true;
+          upstream_timeout = "10s";
+          private_networks = [ ];
+          use_private_ptr_resolvers = true;
+          local_ptr_upstreams = [ ];
+          use_dns64 = false; # TODO
+          dns64_prefixes = [ ];
+          serve_http3 = false; # TODO
+          use_http3_upstreams = false; # TODO
+          # Never false unless one or more encrypted protocols enabled
+          serve_plain_dns = true;
+          hostsfile_enabled = true;
+        };
+        tls = {
+          enabled = false;
+          server_name = "";
+          force_https = false;
+          port_https = 443;
+          port_dns_over_tls = 853;
+          port_dns_over_quic = 853;
+          port_dnscrypt = 0;
+          dnscrypt_config_file = "";
+          allow_unencrypted_doh = false;
+          certificate_chain = "";
+          private_key = "";
+          certificate_path = "";
+          private_key_path = "";
+          strict_sni_check = false;
+        };
+        querylog = {
+          dir_path = "";
+          ignored = [ ];
+          interval = "2160h";
+          size_memory = 1000;
+          enabled = true;
+          file_enabled = true;
+        };
+        statistics = {
+          dir_path = "";
+          ignored = [ ];
+          interval = "24h";
+          enabled = true;
+        };
+        filters = [
+          # TODO: More Filters
+          {
+            enabled = true;
+            url = "https://adguardteam.github.io/HostlistsRegistry/assets/filter_1.txt";
+            name = "AdGuard DNS filter";
+            id = 1;
+          }
+          {
+            enabled = true;
+            url = "https://gitlab.com/hagezi/mirror/-/raw/main/dns-blocklists/adblock/multi.txt";
+            name = "Hagezi Normal Blocklist";
+            id = 2;
+          }
+          {
+            enabled = true;
+            url = "https://gitlab.com/hagezi/mirror/-/raw/main/dns-blocklists/adblock/tif.txt";
+            name = "Hagezi TIF Blocklist";
+            id = 3;
+          }
+          {
+            enabled = true;
+            url = "https://gitlab.com/hagezi/mirror/-/raw/main/dns-blocklists/adblock/spam-tlds.txt";
+            name = "Hagezi Most Abused TLDs";
+            id = 4;
+          }
+        ];
+        whitelist_filters = [ ];
+        user_rules = [ ];
+        dhcp = {
+          enabled = false;
+          interface_name = "";
+          local_domain_name = "lan";
+          dhcpv4 = {
+            gateway_ip = "";
+            subnet_mask = "";
+            range_start = "";
+            range_end = "";
+            lease_duration = 86400;
+            icmp_timeout_msec = 1000;
+            options = [ ];
+          };
+          dhcpv6 = {
+            range_start = "";
+            lease_duration = 86400;
+            ra_slaac_only = false;
+            ra_allow_slaac = false;
+          };
+        };
+        filtering = {
+          blocking_ipv4 = "";
+          blocking_ipv6 = "";
+          blocked_services = {
+            schedule.time_zone = "Local";
+            # TODO: Check restrictiveness and adjust
+            ids = [
+              "4chan"
+              "500px"
+              "9gag"
+              "aliexpress"
+              "amazon"
+              "amazon_streaming"
+              "amino"
+              "apple_streaming"
+              "betano"
+              "betfair"
+              "betway"
+              "bigo_live"
+              "bilibili"
+              "blaze"
+              "bluesky"
+              "box"
+              "canais_globo"
+              "claro"
+              "cloudflare"
+              "clubhouse"
+              "coolapk"
+              "crunchyroll"
+              "dailymotion"
+              "deezer"
+              "directvgo"
+              "discoveryplus"
+              "disneyplus"
+              "douban"
+              "dropbox"
+              "espn"
+              "facebook"
+              "fifa"
+              "flickr"
+              "globoplay"
+              "gog"
+              "hulu"
+              "hbomax"
+              "icloud_private_relay"
+              "instagram"
+              "iheartradio"
+              "iqiyi"
+              "kakaotalk"
+              "kik"
+              "leagueoflegends"
+              "lazada"
+              "kook"
+              "line"
+              "linkedin"
+              "lionsgateplus"
+              "mastodon"
+              "mail_ru"
+              "looke"
+              "mercado_libre"
+              "nebula"
+              "nvidia"
+              "nintendo"
+              "netflix"
+              "ok"
+              "olvid"
+              "onlyfans"
+              "pinterest"
+              "paramountplus"
+              "plenty_of_fish"
+              "plex"
+              "qq"
+              "privacy"
+              "pluto_tv"
+              "rakuten_viki"
+              "riot_games"
+              "roblox"
+              "shein"
+              "shopee"
+              "soundcloud"
+              "skype"
+              "snapchat"
+              "telegram"
+              "tiktok"
+              "tidal"
+              "temu"
+              "tinder"
+              "tumblr"
+              "valorant"
+              "twitter"
+              "viber"
+              "vimeo"
+              "vk"
+              "wechat"
+              "wargaming"
+              "voot"
+              "weibo"
+              "wizz"
+              "xiaohongshu"
+              "zhihu"
+              "yy"
+              "imgur"
+            ];
+          };
+          protection_disabled_until = null;
+          safe_search = {
+            enabled = false;
+            bing = true;
+            duckduckgo = true;
+            google = true;
+            pixabay = true;
+            yandex = true;
+            youtube = true;
+          };
+          blocking_mode = "default";
+          parental_block_host = "family-block.dns.adguard.com";
+          safebrowsing_block_host = "standard-block.dns.adguard.com";
+          rewrites = [ ]; # TODO: implement?
+          safebrowsing_cache_size = 1048576;
+          safesearch_cache_size = 1048576;
+          parental_cache_size = 1048576;
+          cache_time = 30;
+          filters_update_interval = 24;
+          blocked_response_ttl = 10;
+          filtering_enabled = true;
+          parental_enabled = false;
+          safebrowsing_enabled = false;
+          protection_enabled = true;
+        };
+        clients = {
+          runtime_sources = {
+            whois = true;
+            arp = true;
+            rdns = true;
+            dhcp = true;
+            hosts = true;
+          };
+          persistent = [ ];
+        };
+        log = {
+          enabled = true;
+          file = "";
+          max_backups = 0;
+          max_size = 100;
+          max_age = 3;
+          compress = false;
+          local_time = false;
+          verbose = false;
+        };
+        os = {
+          group = "";
+          user = "";
+          rlimit_nofile = 0;
+        };
+        schema_version = 28;
+      };
+    };
+
     minecraft-servers = {
       enable = true;
       eula = true;
