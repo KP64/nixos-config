86f7906a0f1470516eb661bb18224474a3b783c1
KP64 <karamalsadeh@hotmail.com>
2024-09-25 22:52:20 +0200

    Change: Btop auto start

--- programs/cli/monitors/btop.nix
+++ programs/cli/monitors/btop.nix
@@ -13,8 +13,6 @@
       enable = true;
       # Allows GPU detection for Nvidia cards
       package = pkgs.btop.override { cudaSupport = true; };
-      # gpu0 not shown by default
-      settings.shown_boxes = "cpu mem net proc gpu0";
     };
   };
 }
