0fc36f2d28137e3843de86243cc204b9854b1dba
KP64 <karamalsadeh@hotmail.com>
2024-09-27 22:33:45 +0200

    Add: SSH Agent

--- system/services/ssh.nix
+++ system/services/ssh.nix
@@ -7,6 +7,8 @@
   options.system.services.ssh.enable = lib.mkEnableOption "Enabled Openssh";
 
   config = lib.mkIf config.system.services.ssh.enable {
+    programs.ssh.startAgent = true;
+
     services.openssh = {
       enable = true;
       startWhenNeeded = true;
