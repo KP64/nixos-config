c26ec7a65bd0aee9b88e2259650f8b9a46eba060
KP64 <karamalsadeh@hotmail.com>
2024-11-04 22:09:47 +0100

    add: persistent sops-nix

--- system/sops.nix
+++ system/sops.nix
@@ -1,14 +1,24 @@
-{ pkgs, inputs, ... }:
+{
+  pkgs,
+  inputs,
+  username,
+  ...
+}:
 {
   imports = [ inputs.sops-nix.nixosModules.sops ];
-  environment.systemPackages = with pkgs; [
-    age
-    rage
-    age-plugin-tpm
-    age-plugin-yubikey
-    gnupg
-    sops
-    ssh-to-pgp
-    ssh-to-age
-  ];
+  environment = {
+    systemPackages = with pkgs; [
+      age
+      rage
+      age-plugin-tpm
+      age-plugin-yubikey
+      gnupg
+      sops
+      ssh-to-pgp
+      ssh-to-age
+    ];
+    persistence."/persist".users.${username}.directories = [
+      ".config/sops/age/keys.txt"
+    ];
+  };
 }
