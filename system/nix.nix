c8efb2bc094b2993a79babcf3b4f35a2f2771fdf
KP64 <karamalsadeh@hotmail.com>
2024-11-15 15:43:42 +0100

    add: nix-community cache

--- system/nix.nix
+++ system/nix.nix
@@ -34,6 +34,8 @@ in
         ];
         auto-optimise-store = true;
         trusted-users = [ username ] ++ cfg.extraTrustedUsers;
+        substituters = [ "https://nix-community.cachix.org" ];
+        trusted-public-keys = [ "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" ];
       };
     };
 
