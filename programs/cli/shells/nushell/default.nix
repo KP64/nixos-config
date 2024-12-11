3c5c84535a20fbc0ea33e6b124b9eeddbeb53413
KP64 <karamalsadeh@hotmail.com>
2024-12-11 15:13:43 +0100

    refactor: Home-Manager NushellPlugins

--- programs/cli/shells/nushell/default.nix
+++ programs/cli/shells/nushell/default.nix
@@ -7,33 +7,6 @@
 }:
 
 let
-  plugins = with pkgs.nushellPlugins; [
-    dbus
-    formats
-    gstat
-    # highlight
-    polars
-    query
-    # skim
-  ];
-
-  activateNuPluginsScript =
-    plugins
-    |> (map (
-      plugin:
-      let
-        plugin_name = builtins.replaceStrings [ "nushell" ] [ "nu" ] (lib.getName plugin);
-      in
-      "plugin add ${plugin}/bin/${plugin_name}"
-    ))
-    |> lib.concatLines
-    |> pkgs.writers.writeNu "activateNuPlugins";
-
-  msgPackz = pkgs.runCommand "nushellMsgPackz" { } ''
-    mkdir -p "$out"
-    ${pkgs.nushell}/bin/nu --plugin-config "$out/plugin.msgpackz" ${activateNuPluginsScript}
-  '';
-
   cfg = config.cli.shells.nushell;
 in
 {
@@ -42,75 +15,78 @@ in
 
   config = lib.mkMerge [
     (lib.mkIf cfg.enable {
-      home-manager.users.${username} = {
-        xdg.configFile."nushell/plugin.msgpackz".source = "${msgPackz}/plugin.msgpackz";
-        programs.nushell = {
-          enable = true;
-          shellAliases = {
-            ll = "ls -l";
-            la = "ls -a";
-            lla = "ls -l -a";
-          };
-          configFile.source = ./config.nu;
-          extraConfig =
-            let
-              nuScriptsDir = "${pkgs.nu_scripts}/share/nu_scripts";
-
-              useAlias = cmd: "use ${nuScriptsDir}/aliases/${cmd}/${cmd}-aliases.nu *";
-
-              completionsDir = "${nuScriptsDir}/custom-completions";
-              useCompletion =
-                cmd:
-                if builtins.isAttrs cmd then
-                  "use ${completionsDir}/${cmd.dir}/${cmd.file}-completions.nu *"
-                else
-                  "use ${completionsDir}/${cmd}/${cmd}-completions.nu *";
-            in
-            ''
-              # Theme
-              source ${nuScriptsDir}/themes/nu-themes/catppuccin-mocha.nu
-
-              # Aliases
-              ${lib.concatMapStringsSep "\n" useAlias [
-                "bat"
-                "docker"
-                "git"
-              ]}
-
-              # Completions
-              ${lib.concatMapStringsSep "\n" useCompletion [
-                "ani-cli"
-                "bat"
-                "cargo"
-                "curl"
-                "docker"
-                "git"
-                "glow"
-                "just"
-                "less"
-                "make"
-                "man"
-                "nix"
-                "npm"
-                "rg"
-                "rustup"
-                "ssh"
-                "tar"
-                {
-                  dir = "tealdeer";
-                  file = "tldr";
-                }
-                "typst"
-                "zellij"
-              ]}
-            '';
+      home-manager.users.${username}.programs.nushell = {
+        enable = true;
+        shellAliases = {
+          ll = "ls -l";
+          la = "ls -a";
+          lla = "ls -l -a";
         };
+        plugins = with pkgs.nushellPlugins; [
+          dbus
+          formats
+          gstat
+          polars
+          query
+        ];
+        configFile.source = ./config.nu;
+        extraConfig =
+          let
+            nuScriptsDir = "${pkgs.nu_scripts}/share/nu_scripts";
+
+            useAlias = cmd: "use ${nuScriptsDir}/aliases/${cmd}/${cmd}-aliases.nu *";
+
+            completionsDir = "${nuScriptsDir}/custom-completions";
+            useCompletion =
+              cmd:
+              if builtins.isAttrs cmd then
+                "use ${completionsDir}/${cmd.dir}/${cmd.file}-completions.nu *"
+              else
+                "use ${completionsDir}/${cmd}/${cmd}-completions.nu *";
+          in
+          ''
+            # Theme
+            source ${nuScriptsDir}/themes/nu-themes/catppuccin-mocha.nu
+
+            # Aliases
+            ${lib.concatMapStringsSep "\n" useAlias [
+              "bat"
+              "docker"
+              "git"
+            ]}
+
+            # Completions
+            ${lib.concatMapStringsSep "\n" useCompletion [
+              "ani-cli"
+              "bat"
+              "cargo"
+              "curl"
+              "docker"
+              "git"
+              "glow"
+              "just"
+              "less"
+              "make"
+              "man"
+              "nix"
+              "npm"
+              "rg"
+              "rustup"
+              "ssh"
+              "tar"
+              {
+                dir = "tealdeer";
+                file = "tldr";
+              }
+              "typst"
+              "zellij"
+            ]}
+          '';
       };
     })
 
     (lib.mkIf config.system.impermanence.enable {
-      environment.persistence."/persist".users.${username}.files =
-        lib.optional cfg.enable ".config/nushell/history.txt";
+      environment.persistence."/persist".users.${username}.files = lib.optional cfg.enable ".config/nushell/history.txt";
     })
   ];
 }
