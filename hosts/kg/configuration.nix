8201dcf65e626e6540e3834099b46d4f5a0f9e8b
KP64 <karamalsadeh@hotmail.com>
2024-12-18 17:13:50 +0100

    add: system lang module

--- hosts/kg/configuration.nix
+++ hosts/kg/configuration.nix
@@ -21,6 +21,7 @@
   system = {
     inherit stateVersion;
     boot.efi.enable = true;
+    language = "de";
     impermanence.enable = true;
     security = {
       uutils-coreutils.enable = true;
@@ -221,22 +222,6 @@
 
   time.timeZone = "Europe/Berlin";
 
-  i18n = {
-    defaultLocale = "en_US.UTF-8";
-    extraLocaleSettings = {
-      LC_ADDRESS = "de_DE.UTF-8";
-      LC_IDENTIFICATION = "de_DE.UTF-8";
-      LC_MEASUREMENT = "de_DE.UTF-8";
-      LC_MONETARY = "de_DE.UTF-8";
-      LC_NAME = "de_DE.UTF-8";
-      LC_NUMERIC = "de_DE.UTF-8";
-      LC_PAPER = "de_DE.UTF-8";
-      LC_TELEPHONE = "de_DE.UTF-8";
-      LC_TIME = "de_DE.UTF-8";
-    };
-  };
-
-  console.keyMap = "de";
   nixpkgs.config.allowUnfree = true;
 
   users.users.${username} = {
--- system/default.nix
+++ system/default.nix
@@ -8,6 +8,7 @@
     ./fonts.nix
     ./home-manager.nix
     ./impermanence.nix
+    ./i18n.nix
     ./nix.nix
     ./sops.nix
     ./wsl.nix
