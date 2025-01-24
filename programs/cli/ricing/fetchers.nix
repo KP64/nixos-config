14011adb770f83135c6bff3a10fb5c8306124918
KP64 <karamalsadeh@hotmail.com>
2025-01-24 18:35:39 +0100

    add: gpufetch

--- programs/cli/ricing/fetchers.nix
+++ programs/cli/ricing/fetchers.nix
@@ -10,12 +10,13 @@
 
   config = lib.mkIf config.cli.ricing.fetchers.enable {
     home-manager.users.${username} = {
-      home.packages = with pkgs; [
-        onefetch
-        cpufetch
-        # TODO: Package
-        # gpufetch # Not available on nixpkgs
-      ];
+      home.packages =
+        with pkgs;
+        [
+          onefetch
+          cpufetch
+        ]
+        ++ lib.optional (pkgs.system == "x86_64-linux") pkgs.gpufetch;
 
       programs.fastfetch.enable = true;
     };
