ec08b26566207001112a6c46020b7aa89b052718
KP64 <karamalsadeh@hotmail.com>
2025-02-01 05:58:56 +0100

    add: greps

--- programs/cli/grep.nix
+++ programs/cli/grep.nix
@@ -1,7 +1,11 @@
 { pkgs, username, ... }:
 {
   home-manager.users.${username} = {
-    home.packages = [ pkgs.igrep ];
+    home.packages = with pkgs; [
+      bingrep
+      igrep
+      sig
+    ];
     programs.ripgrep.enable = true;
   };
 }
