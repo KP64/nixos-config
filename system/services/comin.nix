b6aa89f865f59a6e4b58ba06055268e7ffb70d37
KP64 <karamalsadeh@hotmail.com>
2025-02-02 11:55:30 +0100

    fix(system/comin): too strict hardening with impermanence

--- system/services/comin.nix
+++ system/services/comin.nix
@@ -14,59 +14,25 @@ in
     default = true;
   };
 
-  config = lib.mkIf cfg.enable {
-    services.comin = {
-      enable = true;
-      remotes = [
-        {
-          name = "origin";
-          url = "https://github.com/KP64/nixos-config.git";
-          poller.period = 60 * 5; # 5 min
-          # No testing branch on this remote
-          branches.testing.name = "";
-        }
-      ];
-      # exporter = { }; # TODO: For prometheus
-    };
+  config = lib.mkMerge [
+    (lib.mkIf cfg.enable {
+      services.comin = {
+        enable = true;
+        remotes = [
+          {
+            name = "origin";
+            url = "https://github.com/KP64/nixos-config.git";
+            poller.period = 60 * 5; # 5 min
+            # No testing branch on this remote
+            branches.testing.name = "";
+          }
+        ];
+        # exporter = { }; # TODO: For prometheus
+      };
+    })
 
-    systemd.services.comin.serviceConfig = {
-      DynamicUser = true;
-      UMask = "0077";
-
-      RemoveIPC = true;
-      LockPersonality = true;
-      NoNewPrivileges = true;
-      MemoryDenyWriteExecute = true;
-      CapabilityBoundingSet = "";
-
-      SystemCallArchitectures = "native";
-      SystemCallFilter = [
-        "~ @privileged @resources @debug @obsolete @cpu-emulation @mount"
-      ];
-
-      RestrictSUIDSGID = true;
-      RestrictRealtime = true;
-      RestrictNamespaces = true;
-      RestrictAddressFamilies = [
-        "AF_INET"
-        "AF_INET6"
-      ];
-
-      PrivateDevices = true;
-      PrivateUsers = true;
-      PrivateTmp = true;
-
-      ProcSubset = "pid";
-
-      ProtectProc = "invisible";
-      ProtectSystem = "strict";
-      ProtectKernelModules = true;
-      ProtectKernelTunables = true;
-      ProtectHome = true;
-      ProtectClock = true;
-      ProtectKernelLogs = true;
-      ProtectControlGroups = true;
-      ProtectHostname = true;
-    };
-  };
+    (lib.mkIf config.isImpermanenceEnabled {
+      environment.persistence."/persist".directories = lib.optional cfg.enable "/var/lib/comin";
+    })
+  ];
 }
