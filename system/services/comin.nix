361a29bf9bd00a9a61f3c03d1ed67d614425f795
KP64 <karamalsadeh@hotmail.com>
2025-02-02 09:44:26 +0100

    Testing SSH signing

--- system/services/comin.nix
+++ system/services/comin.nix
@@ -28,5 +28,45 @@ in
       ];
       # exporter = { }; # TODO: For prometheus
     };
+
+    systemd.services.comin.serviceConfig = {
+      DynamicUser = true;
+      UMask = "0077";
+
+      RemoveIPC = true;
+      LockPersonality = true;
+      NoNewPrivileges = true;
+      MemoryDenyWriteExecute = true;
+      CapabilityBoundingSet = "";
+
+      SystemCallArchitectures = "native";
+      SystemCallFilter = [
+        "~ @privileged @resources @debug @obsolete @cpu-emulation @mount"
+      ];
+
+      RestrictSUIDSGID = true;
+      RestrictRealtime = true;
+      RestrictNamespaces = true;
+      RestrictAddressFamilies = [
+        "AF_INET"
+        "AF_INET6"
+      ];
+
+      PrivateDevices = true;
+      PrivateUsers = true;
+      PrivateTmp = true;
+
+      ProcSubset = "pid";
+
+      ProtectProc = "invisible";
+      ProtectSystem = "strict";
+      ProtectKernelModules = true;
+      ProtectKernelTunables = true;
+      ProtectHome = true;
+      ProtectClock = true;
+      ProtectKernelLogs = true;
+      ProtectControlGroups = true;
+      ProtectHostname = true;
+    };
   };
 }
