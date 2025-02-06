781c5bf628f52bc534fd431b666605223826eb29
KP64 <karamalsadeh@hotmail.com>
2025-02-06 14:04:27 +0100

    chore(programs/discord): remove unused plugins

--- programs/gaming/discord.nix
+++ programs/gaming/discord.nix
@@ -26,7 +26,6 @@ in
             plugins = {
               accountPanelServerProfile.enable = true;
               alwaysAnimate.enable = true;
-              alwaysExpandRoles.enable = true;
               anonymiseFileNames.enable = true;
               betterFolders = {
                 enable = true;
@@ -35,7 +34,6 @@ in
                 closeAllHomeButton = true;
                 closeOthers = true;
               };
-              betterGifAltText.enable = true;
               betterRoleContext.enable = true;
               betterRoleDot = {
                 enable = true;
@@ -52,45 +50,31 @@ in
               clearURLs.enable = true;
               consoleJanitor.enable = true;
               copyFileContents.enable = true;
-              copyUserURLs.enable = true;
               dearrow.enable = true;
               disableCallIdle.enable = true;
-              dontRoundMyTimestamps.enable = true;
-              emoteCloner.enable = true;
               fakeNitro.enable = true;
-              fakeProfileThemes.enable = true;
               favoriteEmojiFirst.enable = true;
               fixCodeblockGap.enable = true;
               fixImagesQuality.enable = true;
-              fixSpotifyEmbeds.enable = true;
-              fixYoutubeEmbeds.enable = true;
-              friendInvites.enable = true;
               friendsSince.enable = true;
               fullSearchContext.enable = true;
               fullUserInChatbox.enable = true;
               gameActivityToggle.enable = true;
               gifPaste.enable = true;
-              greetStickerPicker.enable = true;
-              hideAttachments.enable = true;
               imageZoom.enable = true;
               implicitRelationships = {
                 enable = true;
                 sortByAffinity = true;
               };
-              memberCount.enable = true;
               mentionAvatars.enable = true;
               messageClickActions.enable = true;
-              messageLatency.enable = true;
               messageLinkEmbeds = {
                 enable = true;
                 messageBackgroundColor = true;
               };
               messageLogger.enable = true;
-              messageTags.enable = true;
               mutualGroupDMs.enable = true;
               newGuildSettings.enable = true;
-              noOnboardingDelay.enable = true;
-              noScreensharePreview.enable = true;
               noUnblockToJump.enable = true;
               nsfwGateBypass.enable = true;
               openInApp = {
@@ -99,11 +83,8 @@ in
                 steam = true;
                 epic = true;
               };
-              pictureInPicture.enable = true;
               pinDMs.enable = true;
-              platformIndicators.enable = true;
               quickMention.enable = true;
-              readAllNotificationsButton.enable = true;
               relationshipNotifier = {
                 enable = true;
                 notices = true;
@@ -116,16 +97,12 @@ in
                 theme = lib.optionalString config.isCatppuccinEnabled "https://raw.githubusercontent.com/shikijs/textmate-grammars-themes/refs/heads/main/packages/tm-themes/themes/catppuccin-mocha.json";
               };
               showConnections.enable = true;
-              showHiddenChannels.enable = true;
-              showMeYourName.enable = true;
               silentTyping = {
                 enable = true;
                 showIcon = true;
               };
               stickerPaste.enable = true;
               streamerModeOnStream.enable = true;
-              superReactionTweaks.enable = true;
-              themeAttributes.enable = true;
               typingIndicator = {
                 enable = true;
                 includeMutedChannels = true;
@@ -133,13 +110,10 @@ in
               };
               typingTweaks.enable = true;
               userVoiceShow.enable = true;
-              USRBG.enable = true;
               voiceChatDoubleClick.enable = true;
-              voiceMessages.enable = true;
+              webScreenShareFixes.enable = true; # Vesktop only
               whoReacted.enable = true;
               youtubeAdblock.enable = true;
-              # Vesktop Exclusive
-              webScreenShareFixes.enable = true;
             };
           };
         };
