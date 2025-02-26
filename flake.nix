2ff5dab09370e0edff7377a8ca55f618fe3abb60
KP64 <karamalsadeh@hotmail.com>
2025-02-26 18:01:38 +0100

    refactor: system import
    no need to respecify the systems the systems flake is present as is.

--- flake.nix
+++ flake.nix
@@ -264,10 +264,7 @@
   outputs =
     inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-      ];
+      systems = import inputs.systems;
 
       flake =
         let
