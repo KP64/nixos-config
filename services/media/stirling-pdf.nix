f4298b0295d7beb92d867a628e10b945ebe1d727
KP64 <karamalsadeh@hotmail.com>
2025-03-30 21:57:20 +0200

    refactor(services/stirling-pdf): Uncontainerize

--- services/media/stirling-pdf.nix
+++ services/media/stirling-pdf.nix
@@ -1,31 +1,14 @@
 { config, lib, ... }:
 let
   cfg = config.services.media.stirling-pdf;
-  port = 4300;
+
+  port = config.services.stirling-pdf.environment.SERVER_PORT;
   stirlingPort = toString port;
 in
 {
   options.services.media.stirling-pdf.enable = lib.mkEnableOption "Stirling-pdf";
 
-  # Docker Container to bypass weird build failures
-  # with the nixpkgs module
   config = lib.mkIf cfg.enable {
-    virtualisation.oci-containers.containers.stirling-pdf = {
-      image = "stirlingtools/stirling-pdf:latest";
-      ports = [ "${stirlingPort}:8080" ];
-      volumes = map (p: "./StirlingPDF/${p}") [
-        "trainingData:/usr/share/tessdata"
-        "extraConfigs:/configs"
-        "customFiles:/customFiles/"
-        "logs:/logs/"
-        "pipeline:/pipeline/"
-      ];
-      environment = {
-        DOCKER_ENABLE_SECURITY = "false";
-        LANGS = "de_DE";
-      };
-    };
-
     services = {
       traefik.dynamicConfigOptions.http = {
         routers.stirling-pdf = {
@@ -33,6 +16,11 @@ in
           service = "stirling-pdf";
         };
         services.stirling-pdf.loadBalancer.servers = [ { url = "http://localhost:${stirlingPort}"; } ];
+      };
+
+      stirling-pdf = {
+        enable = true;
+        environment.SERVER_PORT = 4300;
       };
 
       tor.relay.onionServices.stirling-pdf.map = [
