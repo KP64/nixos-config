ad713f177878d94acda677070aa3a2e90d2868bc
KP64 <karamalsadeh@hotmail.com>
2025-04-16 17:32:17 +0200

    update outdated tp ssh keys

--- hosts/nixos/x86_64-linux/aladdin/users/kg/default.nix
+++ hosts/nixos/x86_64-linux/aladdin/users/kg/default.nix
@@ -4,7 +4,7 @@
     isNormalUser = true;
     hashedPasswordFile = config.sops.secrets."users/kg/password".path;
     openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFqboCBFR7zCUKnUoIIXbYh42muPCKNXZ+g6cp/KXQaX tp@tp"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMH6w/Q19oTmsf88Sksonk5A07wcNAloXaChlsKIEO83 tp@sindbad"
     ];
     extraGroups = [
       "networkmanager"
