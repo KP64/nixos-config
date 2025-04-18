e9c42aa3a5a13879939e6aa410f9edb93fe19af0
KP64 <karamalsadeh@hotmail.com>
2025-04-18 20:31:07 +0200

    chore(modules/home): broot vim mode by default

--- modules/home/file-managers/broot.nix
+++ modules/home/file-managers/broot.nix
@@ -3,8 +3,10 @@ let
   cfg = config.file-managers.broot;
 in
 {
-  # TODO: Catppuccin theme
   options.file-managers.broot.enable = lib.mkEnableOption "Broot";
 
-  config.programs.broot = { inherit (cfg) enable; };
+  config.programs.broot = {
+    inherit (cfg) enable;
+    settings.modal = true;
+  };
 }
