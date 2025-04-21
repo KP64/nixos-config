9e61cdde037d64d65749928fafaafadf752b7437
KP64 <karamalsadeh@hotmail.com>
2025-04-21 23:43:39 +0200

    chore(modules/home): always install wl-clipboard

--- modules/home/desktop/copyq.nix
+++ modules/home/desktop/copyq.nix
@@ -7,8 +7,8 @@
 {
   options.desktop.services.copyq.enable = lib.mkEnableOption "Copyq";
 
-  config = lib.mkIf config.desktop.services.copyq.enable {
-    home.packages = [ pkgs.wl-clipboard-rs ];
-    services.copyq.enable = true;
-  };
+  config = lib.mkMerge [
+    { home.packages = [ pkgs.wl-clipboard-rs ]; }
+    (lib.mkIf config.desktop.services.copyq.enable { services.copyq.enable = true; })
+  ];
 }
