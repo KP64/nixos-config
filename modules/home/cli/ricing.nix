e2826bf963294e321c448ba5e346f1f8a15fed47
KP64 <karamalsadeh@hotmail.com>
2025-05-10 23:45:44 +0200

    chore(modules/home): remove rain ricing

--- modules/home/cli/ricing.nix
+++ modules/home/cli/ricing.nix
@@ -7,22 +7,6 @@
 }:
 let
   cfg = config.cli.ricing;
-
-  rain = pkgs.writeShellApplication {
-    name = "rain";
-    text = ''
-      trap "exit 0" SIGINT
-
-      while true; do
-        if read -rsn1 -t 0.01 key && [[ $key == q ]]; then
-          break
-        fi
-
-        c=$(( $(date +%s%N) % 8 ))
-        printf "\033[1;3%sm." "$c"
-      done
-    '';
-  };
 in
 {
   options.cli.ricing.enable = lib.mkEnableOption "All ricing";
@@ -48,7 +32,6 @@ in
         tenki
         toilet
         tty-clock
-        rain
       ]);
   };
 }
