abd8064405526da7d1e7b344d9a5ad7e42eba852
KP64 <karamalsadeh@hotmail.com>
2025-06-09 22:07:55 +0200

    chore(modules/nixos): use resolved fallbackDns

--- modules/nixos/services/networking/resolved.nix
+++ modules/nixos/services/networking/resolved.nix
@@ -1,26 +1,29 @@
 { config, lib, ... }:
 {
   networking = lib.mkIf (config ? wsl && !config.wsl.enable) {
-    nameservers =
-      [
-        # Quad9
-        "9.9.9.9"
-        "149.112.112.112"
-        "2620:fe::fe"
-        "2620:fe::9"
-      ]
-      ++ [
-        # Cloudflare
-        "1.1.1.1"
-        "1.0.0.1"
-        "2606:4700:4700::1111"
-        "2606:4700:4700::1001"
-      ];
+    nameservers = [
+      # Quad9
+      "9.9.9.9"
+      "2620:fe::fe"
+
+      # Cloudflare
+      "1.1.1.1"
+      "2606:4700:4700::1111"
+    ];
   };
 
   services.resolved = {
     dnssec = "true";
     dnsovertls = "true";
     domains = [ "~." ];
+    fallbackDns = [
+      # Quad9
+      "149.112.112.112"
+      "2620:fe::9"
+
+      # Cloudflare
+      "1.0.0.1"
+      "2606:4700:4700::1001"
+    ];
   };
 }
