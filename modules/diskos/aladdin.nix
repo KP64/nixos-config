f500581ce15c7b9ac7bde56df4d256c82b62817a
KP64 <karamalsadeh@hotmail.com>
2025-09-18 19:41:06 +0200

    chore: aladdin disko lvm to btrfs

--- modules/diskos/aladdin.nix
+++ modules/diskos/aladdin.nix
@@ -1,73 +1,33 @@
 {
   # TODO: Improve this A LOT once disko fleshes out their module.
-  # TODO: Remove lvm -> standard btrfs
   flake.diskoConfigurations.aladdin = {
-    disko.devices = {
-      disk.main = {
-        device = "/dev/sda";
-        type = "disk";
-        content = {
-          type = "gpt";
-          partitions = {
-            ESP = {
-              name = "ESP";
-              size = "512M";
-              type = "EF00";
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
-                mountOptions = [
-                  "umask=0077"
-                  "noatime"
-                ];
-              };
-            };
-            swap = {
-              size = "4G";
-              content = {
-                type = "swap";
-                resumeDevice = true;
-              };
-            };
-            root = {
-              name = "root";
-              size = "100%";
-              content = {
-                type = "lvm_pv";
-                vg = "root_vg";
-              };
+    disko.devices.disk.main = {
+      type = "disk";
+      device = "/dev/sda";
+      content = {
+        type = "gpt";
+        partitions = {
+          ESP = {
+            priority = 1;
+            name = "ESP";
+            size = "1G";
+            type = "EF00";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+              mountOptions = [ "umask=0077" ];
             };
           };
-        };
-      };
-      lvm_vg.root_vg = {
-        type = "lvm_vg";
-        lvs.root = {
-          size = "100%FREE";
-          content = {
-            type = "btrfs";
-            extraArgs = [ "-f" ];
-            subvolumes = {
-              "/root" = {
-                mountpoint = "/";
-                mountOptions = [ "noatime" ];
-              };
-
-              "/persist" = {
-                mountpoint = "/persist";
-                mountOptions = [
-                  "subvol=persist"
-                  "noatime"
-                ];
-              };
-
-              "/nix" = {
-                mountpoint = "/nix";
-                mountOptions = [
-                  "subvol=nix"
-                  "noatime"
-                ];
+          root = {
+            size = "100%";
+            content = {
+              type = "btrfs";
+              extraArgs = [ "-f" ];
+              subvolumes = {
+                "/root".mountpoint = "/";
+                "/home".mountpoint = "/home";
+                "/nix".mountpoint = "/nix";
               };
             };
           };
