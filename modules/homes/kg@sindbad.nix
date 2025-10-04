3965b667e82208ffd0cbcfd7703ccb594ecf3195
KP64 <karamalsadeh@hotmail.com>
2025-10-04 21:53:48 +0200

    chore(homes/kg@sindbad): uninstall hyprland

--- modules/homes/kg@sindbad.nix
+++ modules/homes/kg@sindbad.nix
@@ -1,15 +1,22 @@
 toplevel: {
   # FIXME: Fonts aren't working. Even with fontconfig enabled :/
-  flake.modules.homeManager."kg@sindbad" = {
-    imports = with toplevel.config.flake.modules.homeManager; [
-      desktop
-      users-kg
-    ];
+  flake.modules.homeManager."kg@sindbad" =
+    { config, pkgs, ... }:
+    {
+      imports = with toplevel.config.flake.modules.homeManager; [
+        desktop
+        users-kg
+      ];
 
-    wayland.windowManager.hyprland.portalPackage = null;
+      programs.kitty.package = config.lib.nixGL.wrap pkgs.kitty;
 
-    # TODO: Revert once https://github.com/nix-community/home-manager/issues/7027 is fixed
-    #       This will need the user to install hyprlock via pacman
-    programs.hyprlock.package = null;
-  };
+      wayland.windowManager.hyprland = {
+        package = null; # NOTE: Works with HM, but SDDM won't find it.
+        portalPackage = null;
+      };
+
+      # TODO: Revert once https://github.com/nix-community/home-manager/issues/7027 is fixed
+      #       This will need the user to install hyprlock via pacman
+      programs.hyprlock.package = null;
+    };
 }
