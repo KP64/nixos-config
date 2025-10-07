d79e29d488e7703c2e64b2edef5f26cef6eb528a
KP64 <karamalsadeh@hotmail.com>
2025-10-07 23:32:39 +0200

    feat(hosts/mahdi): add karakeep

---
+++ modules/hosts/mahdi/services/karakeep.nix
@@ -0,0 +1,43 @@
+{
+  flake.modules.nixos.hosts-mahdi =
+    { config, lib, pkgs, ... }:
+    {
+      services.traefik.dynamicConfigOptions.http = {
+        routers.karakeep = {
+          rule = "Host(`karakeep.${config.networking.domain}`)";
+          service = "karakeep";
+        };
+        services.karakeep.loadBalancer.servers = [
+          { url = "http://localhost:${toString config.services.karakeep.extraEnvironment.PORT}"; }
+        ];
+      };
+
+      services.karakeep = {
+        enable = true;
+        browser = {
+          port = 9222;
+          # Default is chromium for whatever reason
+          exe = lib.getExe pkgs.ungoogled-chromium;
+        };
+        extraEnvironment = {
+          NEXTAUTH_URL = "https://karakeep.${config.networking.domain}";
+          PORT = "43547";
+          RATE_LIMITING_ENABLED = "true";
+          DB_WAL_MODE = "true";
+          DISABLE_SIGNUPS = "true";
+
+          OLLAMA_BASE_URL = "http://localhost:${toString config.services.ollama.port}";
+          OLLAMA_KEEP_ALIVE = "5m";
+          # NOTE: The whole name is needed.
+          #       llama3.2 alone isn't enough.
+          INFERENCE_TEXT_MODEL = "llama3.2:3b";
+          INFERENCE_IMAGE_MODEL = "gemma3:1b";
+          EMBEDDING_TEXT_MODEL = "embeddinggemma:300m";
+
+          CRAWLER_FULL_PAGE_SCREENSHOT = "true";
+          CRAWLER_FULL_PAGE_ARCHIVE = "true";
+          CRAWLER_VIDEO_DOWNLOAD = "true";
+        };
+      };
+    };
+}
