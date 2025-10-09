1b4e9bd7b87a17d3971c092f8442ae65c26fac32
KP64 <karamalsadeh@hotmail.com>
2025-10-09 16:11:58 +0200

    fix(users/kg): wrong hyprlock face extension

--- modules/users/kg/dots/hyprlock.nix
+++ modules/users/kg/dots/hyprlock.nix
@@ -1,7 +1,7 @@
 { inputs, ... }:
 {
   flake.modules.homeManager.users-kg = {
-    home.file.".face".source = builtins.path { path = ../pfp.jp; };
+    home.file.".face".source = builtins.path { path = ../pfp.jpg; };
     xdg.configFile."background".source = builtins.path {
       path = inputs.self + /assets/wallpapers/catppuccin/cat-vibin.png;
     };
