b1ef0a89e432d27f2b8ab9f7f74e2f0d1c0364d7
KP64 <karamalsadeh@hotmail.com>
2025-10-18 20:35:36 +0200

    feat(hosts/mahdi): add atuin

---
+++ modules/hosts/mahdi/services/atuin.nix
@@ -0,0 +1,21 @@
+{
+  flake.modules.nixos.hosts-mahdi =
+    { config, ... }:
+    {
+      services.nginx.virtualHosts."atuin.${config.networking.domain}" = {
+        enableACME = true;
+        acmeRoot = null;
+        onlySSL = true;
+        kTLS = true;
+        locations."/" = {
+          proxyPass = "http://localhost:${toString config.services.atuin.port}";
+        };
+      };
+
+      services.atuin = {
+        enable = true;
+        port = 33195;
+        openRegistration = true;
+      };
+    };
+}
