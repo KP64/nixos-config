d62b545d2f8479d5c255323b7a566e19d7abde6d
KP64 <karamalsadeh@hotmail.com>
2025-10-18 21:36:38 +0200

    feat(hosts/mahdi): add scrutiny

---
+++ modules/hosts/mahdi/services/scrutiny.nix
@@ -0,0 +1,24 @@
+{
+  # TODO: Let this be public once kanidm works
+  flake.modules.nixos.hosts-mahdi =
+    { config, ... }:
+    {
+      # services.nginx.virtualHosts."scrutiny.${config.networking.domain}" = {
+      #   enableACME = true;
+      #   acmeRoot = null;
+      #   onlySSL = true;
+      #   kTLS = true;
+      #   locations."/" = {
+      #     proxyPass = "http://localhost:${toString config.services.scrutiny.settings.web.listen.port}";
+      #   };
+      # };
+
+      services.scrutiny = {
+        enable = true;
+        openFirewall = true;
+        settings = {
+          web.listen.port = 44617;
+        };
+      };
+    };
+}
