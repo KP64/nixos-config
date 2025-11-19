67941bcc1f44d1de653d043551edd9ad563556bc
KP64 <karamalsadeh@hotmail.com>
2025-11-19 15:14:03 +0100

    chore(homes): add todo

--- modules/homes/_additional_hosts.nix
+++ modules/homes/_additional_hosts.nix
@@ -1,4 +1,6 @@
 {
+  # TODO: Replace with flake-parts module
+  #        - Custom Flake-Parts Hosts option needed
   sindbad = {
     system = "x86_64-linux";
     modules = [ { targets.genericLinux.enable = true; } ];
