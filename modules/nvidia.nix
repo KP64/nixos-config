79e920ad3f538b74d08a9164d7331b32603058b4
KP64 <karamalsadeh@hotmail.com>
2025-12-02 01:07:31 +0100

    chore(modules): add nvidia cache

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -1,6 +1,10 @@
 {
   flake.modules.nixos.nvidia = {
     nixpkgs.config.cudaSupport = true;
+    nix.settings = {
+      substituters = [ "https://cache.nixos-cuda.org" ];
+      trusted-public-keys = [ "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M=" ];
+    };
 
     services.xserver.videoDrivers = [ "nvidia" ];
 
