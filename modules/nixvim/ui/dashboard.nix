8e6a83980c66e0891ce2bdc2d0904e2dd95d3027
KP64 <karamalsadeh@hotmail.com>
2025-12-16 00:58:32 +0100

    chore(modules/nixvim): remove todo

--- modules/nixvim/ui/dashboard.nix
+++ modules/nixvim/ui/dashboard.nix
@@ -16,9 +16,6 @@
             header = config.neovim-dashboard;
             packages.enable = false;
             footer = lib.nixvim.utils.emptyTable;
-            # TODO: Open issue that shortcuts can not be disabled.
-            # This is because trying to set `shortcut` to an empty
-            # table is not allowed per type signature of the nix option.
             shortcut = [
               {
                 action = "Telescope find_files cwd=";
