c87e9a59ca27f72c80619cb464961419af798247
KP64 <karamalsadeh@hotmail.com>
2026-01-16 15:23:09 +0100

    chore(hosts/mahdi): add clamav settings

--- modules/hosts/mahdi/services/clamav.nix
+++ modules/hosts/mahdi/services/clamav.nix
@@ -1,7 +1,18 @@
 {
   flake.modules.nixos.hosts-mahdi = {
     services.clamav = {
-      daemon.enable = true;
+      clamonacc.enable = true;
+      daemon = {
+        enable = true;
+        settings = {
+          DetectPUA = true;
+          ExtendedDetectionInfo = true;
+          FollowDirectorySymlinks = true;
+          FollowFileSymlinks = true;
+          LogSyslog = true;
+          LogTime = true;
+        };
+      };
       updater.enable = true;
       scanner.enable = true;
       fangfrisch.enable = true;
