2da844d70d4caa08e1bfa72c02477c2d36f6b406
KP64 <karamalsadeh@hotmail.com>
2026-01-16 15:21:57 +0100

    fix(hosts/mahdi): unreachable code server

--- modules/hosts/mahdi/services/code-server.nix
+++ modules/hosts/mahdi/services/code-server.nix
@@ -9,12 +9,13 @@
         kTLS = true;
         locations."/" = {
           proxyWebsockets = true;
-          proxyPass = "http://127.0.0.1:${toString config.services.code-server.port}";
+          proxyPass = "http://${config.services.code-server.host}:${toString config.services.code-server.port}";
         };
       };
 
       services.code-server = {
         enable = true;
+        host = "127.0.0.1";
         disableTelemetry = true;
         disableUpdateCheck = true;
         hashedPassword = "$argon2i$v=19$m=4096,t=3,p=1$SzRETTV4Z0VxUDF3WU5Pa2d1UmxOaEZxNkpJPQ$inoHfYan+i8IHSK4Dv0l7NtEGZr7HDrsysCYOW3GN3U";
