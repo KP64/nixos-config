7af45a0854a108452f9b1eb6145c3a2d37412687
KP64 <karamalsadeh@hotmail.com>
2026-01-22 19:36:37 +0100

    chore(modules): add time todo and note

--- modules/time.nix
+++ modules/time.nix
@@ -1,32 +1,28 @@
 {
-  flake.modules.nixos.time =
-    { lib, ... }:
-    {
-      services.ntpd-rs = {
-        enable = true;
-        settings = {
-          source =
-            map
-              (lib.mergeAttrs {
-                # We only care about nts servers ;D
-                # (if we exclude the nixos pool that is)
-                mode = "nts";
-                # Upgrade to Draft NTPv5 if possible :)
-                ntp-version = "auto";
-              })
-              (
-                [
-                  { address = "nts.netnod.se"; }
-                  { address = "time.cloudflare.com"; }
-                ]
-                ++ (
-                  4
-                  |> builtins.genList (i: {
-                    address = "ptbtime${toString <| i + 1}.ptb.de";
-                  })
-                )
-              );
-        };
-      };
+  # TODO: RTC for ALL Pis. Needed for Sane time on startup.
+  #       Benefit: Learning Experience & Uncompromising DNSSEC and DoT
+  # NOTE: Only enable if DNS isn't too strict or Device has an RTC.
+  #       If the DNS strictly needs DoT and DNSSEC and there is no RTC then
+  #       then no time can be fetched. Without the correct time
+  #       DoT and DNSSEC fail causing a deadlock.
+  flake.modules.nixos.time = {
+    services.ntpd-rs = {
+      enable = true;
+      settings.source =
+        map
+          (address: {
+            # We only care about nts servers ;D
+            # (if we exclude the nixos pool that is)
+            mode = "nts";
+            inherit address;
+          })
+          (
+            [
+              "nts.netnod.se"
+              "time.cloudflare.com"
+            ]
+            ++ (builtins.genList (i: "ptbtime${toString (i + 1)}.ptb.de") 4)
+          );
     };
+  };
 }
