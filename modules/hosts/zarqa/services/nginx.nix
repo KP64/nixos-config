78455bfb4ea7d04de1689e310db191e5db8cb51f
KP64 <karamalsadeh@hotmail.com>
2026-02-23 16:15:58 +0100

    feat(hosts/zarqa): add reverse proxy and acme

---
+++ modules/hosts/zarqa/services/nginx.nix
@@ -0,0 +1,37 @@
+{
+  flake.modules.nixos.hosts-mahdi =
+    {
+      config,
+      lib,
+      pkgs,
+      ...
+    }:
+    {
+      networking.firewall =
+        let
+          ssl = lib.optional config.services.nginx.enable config.services.nginx.defaultSSLListenPort;
+        in
+        {
+          allowedTCPPorts = ssl;
+          # QUIC uses UDP
+          allowedUDPPorts = ssl;
+        };
+
+      # NOTE: Amazing Websites:
+      #  - https://securityheaders.com/
+      #  - https://www.ssllabs.com/
+      services.nginx = {
+        enable = true;
+
+        package = pkgs.nginxMainline;
+        enableQuicBPF = true;
+
+        recommendedGzipSettings = true;
+        recommendedOptimisation = true;
+        recommendedProxySettings = true;
+        recommendedTlsSettings = true;
+        recommendedUwsgiSettings = true;
+        recommendedBrotliSettings = true;
+      };
+    };
+}
