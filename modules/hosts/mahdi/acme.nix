b4c14c9603d7b7bc82feefdfe5be49ade39f5700
KP64 <karamalsadeh@hotmail.com>
2026-02-26 15:01:00 +0100

    chore(hosts/mahdi)!: revert to porkbun for now
    Having waaaayyy too many issues with the rpis. Use hickory dns later when everything works as expected.

--- modules/hosts/mahdi/acme.nix
+++ modules/hosts/mahdi/acme.nix
@@ -1,5 +1,38 @@
 {
   flake.modules.nixos.hosts-mahdi =
+    # TODO: Reenable once custom Nameservers are fully functional
+    # { config, ... }:
+    # {
+    #   sops.secrets =
+    #     let
+    #       owner = config.users.users.acme.name;
+    #     in
+    #     {
+    #       "rfc2136/nameserver" = { inherit owner; };
+    #       "rfc2136/tsig_algorithm" = { inherit owner; };
+    #       "rfc2136/tsig_key" = { inherit owner; };
+    #       "rfc2136/tsig_secret" = { inherit owner; };
+    #     };
+    #
+    #   security.acme = {
+    #     acceptTerms = true;
+    #     defaults = {
+    #       inherit (config.invisible) email;
+    #       dnsResolver = "1.1.1.1:53";
+    #       dnsProvider = "rfc2136";
+    #       credentialFiles =
+    #         let
+    #           inherit (config.sops) secrets;
+    #         in
+    #         {
+    #           RFC2136_NAMESERVER_FILE = secrets."rfc2136/nameserver".path;
+    #           RFC2136_TSIG_ALGORITHM_FILE = secrets."rfc2136/tsig_algorithm".path;
+    #           RFC2136_TSIG_KEY_FILE = secrets."rfc2136/tsig_key".path;
+    #           RFC2136_TSIG_SECRET_FILE = secrets."rfc2136/tsig_secret".path;
+    #         };
+    #     };
+    #   };
+    # };
     { config, ... }:
     {
       sops.secrets =
@@ -7,27 +40,22 @@
           owner = config.users.users.acme.name;
         in
         {
-          "rfc2136/nameserver" = { inherit owner; };
-          "rfc2136/tsig_algorithm" = { inherit owner; };
-          "rfc2136/tsig_key" = { inherit owner; };
-          "rfc2136/tsig_secret" = { inherit owner; };
+          "porkbun/api_key" = { inherit owner; };
+          "porkbun/secret_api_key" = { inherit owner; };
         };
 
       security.acme = {
         acceptTerms = true;
         defaults = {
           inherit (config.invisible) email;
-          dnsResolver = "1.1.1.1:53";
-          dnsProvider = "rfc2136";
+          dnsProvider = "porkbun";
           credentialFiles =
             let
               inherit (config.sops) secrets;
             in
             {
-              RFC2136_NAMESERVER_FILE = secrets."rfc2136/nameserver".path;
-              RFC2136_TSIG_ALGORITHM_FILE = secrets."rfc2136/tsig_algorithm".path;
-              RFC2136_TSIG_KEY_FILE = secrets."rfc2136/tsig_key".path;
-              RFC2136_TSIG_SECRET_FILE = secrets."rfc2136/tsig_secret".path;
+              PORKBUN_API_KEY_FILE = secrets."porkbun/api_key".path;
+              PORKBUN_SECRET_API_KEY_FILE = secrets."porkbun/secret_api_key".path;
             };
         };
       };
