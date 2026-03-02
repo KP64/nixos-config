4cffffe9a4d34a585b766e34dc3c3a87a9ea001c
KP64 <karamalsadeh@hotmail.com>
2026-03-02 18:06:18 +0100

    feat: pkg anonymousoverflow

---
+++ modules/services/anonymousoverflow.nix
@@ -0,0 +1,99 @@
+{ moduleWithSystem, ... }:
+{
+  flake.modules.nixos.anonymousoverflow = moduleWithSystem (
+    { config, ... }:
+    nixos@{ lib, ... }:
+    let
+      cfg = nixos.config.services.anonymousoverflow;
+    in
+    {
+      options.services.anonymousoverflow = {
+        enable = lib.mkEnableOption "anonymousoverflow";
+        package = lib.mkPackageOption config.packages "anonymousoverflow" { };
+        host = lib.mkOption {
+          type = with lib.types; nullOr nonEmptyStr;
+          default = "127.0.0.1";
+          example = "0.0.0.0";
+          description = "Where it should listen.";
+        };
+        port = lib.mkOption {
+          type = lib.types.port;
+          default = 5768;
+          example = 8080;
+          description = "The port that Anonymousoverflow will listen on.";
+        };
+        openFirewall = lib.mkOption {
+          type = lib.types.bool;
+          default = false;
+          example = true;
+          description = "Whether to open the Anonymousoverflow port in the firewall.";
+        };
+        appUrl = lib.mkOption {
+          type = lib.types.nonEmptyStr;
+          default = "http://localhost";
+          example = "https://overflow.example.com";
+        };
+        environmentFile = lib.mkOption {
+          type = lib.types.path;
+          description = ''
+            Path to the file containing the JSWT_SIGNING_SECRET variable.
+            To generate the variable run `openssl rand -base64 32`.
+          '';
+        };
+      };
+
+      config = lib.mkIf cfg.enable {
+        networking.firewall.allowedTCPPorts = lib.optional cfg.openFirewall cfg.port;
+
+        systemd.services.anonymousoverflow = {
+          description = "Private alternative front-end for Genius";
+          after = [ "network.target" ];
+          wantedBy = [ "multi-user.target" ];
+          enableStrictShellChecks = true;
+
+          environment = {
+            HOST = cfg.host;
+            PORT = cfg.port;
+            APP_URL = cfg.appUrl;
+          };
+
+          serviceConfig = {
+            EnvironmentFile = cfg.environmentFile;
+            ExecStart = lib.getExe cfg.package;
+            DynamicUser = true;
+            UMask = "0077";
+            RemoveIPC = true;
+            LockPersonality = true;
+            NoNewPrivileges = true;
+            MemoryDenyWriteExecute = true;
+
+            CapabilityBoundingSet = "";
+            SystemCallArchitectures = "native";
+            SystemCallFilter = [ "~ @privileged @resources @debug @obsolete @cpu-emulation @mount" ];
+
+            RestrictSUIDSGID = true;
+            RestrictRealtime = true;
+            RestrictNamespaces = true;
+            RestrictAddressFamilies = [
+              "AF_INET"
+              "AF_INET6"
+            ];
+            PrivateDevices = true;
+            PrivateUsers = true;
+            PrivateTmp = true;
+            ProcSubset = "pid";
+            ProtectProc = "invisible";
+            ProtectSystem = "strict";
+            ProtectKernelModules = true;
+            ProtectKernelTunables = true;
+            ProtectHome = true;
+            ProtectClock = true;
+            ProtectKernelLogs = true;
+            ProtectControlGroups = true;
+            ProtectHostname = true;
+          };
+        };
+      };
+    }
+  );
+}
