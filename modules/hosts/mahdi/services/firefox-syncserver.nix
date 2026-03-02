6364ee0222fdcd7ddd5d649542497675a89422ad
KP64 <karamalsadeh@hotmail.com>
2026-03-02 21:10:00 +0100

    feat(hosts/mahdi): add firefox syncserver

---
+++ modules/hosts/mahdi/services/firefox-syncserver.nix
@@ -0,0 +1,39 @@
+{
+  flake.modules.nixos.hosts-mahdi =
+    {
+      config,
+      lib,
+      pkgs,
+      ...
+    }:
+    lib.mkMerge [
+      (lib.mkIf config.services.firefox-syncserver.enable {
+        sops.secrets."firefox-syncserver.env" = { };
+        services = {
+          mysql.package = pkgs.mariadb;
+
+          nginx.virtualHosts.${config.services.firefox-syncserver.singleNode.hostname} =
+            lib.mkIf config.services.firefox-syncserver.singleNode.enableNginx
+              {
+                forceSSL = lib.mkForce false;
+                onlySSL = true;
+                acmeRoot = null;
+                kTLS = true;
+              };
+        };
+      })
+      {
+        services.firefox-syncserver = {
+          enable = true;
+          secrets = config.sops.secrets."firefox-syncserver.env".path;
+          # settings = { };
+          singleNode = {
+            enable = true;
+            hostname = "firefox-sync.${config.networking.domain}";
+            enableTLS = true;
+            enableNginx = true;
+          };
+        };
+      }
+    ];
+}
