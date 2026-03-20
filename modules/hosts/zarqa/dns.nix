07134df18f0c61e4417594fa2a20cfd440e315d5
KP64 <karamalsadeh@hotmail.com>
2026-03-20 21:49:37 +0100

    fix(hosts/zarqa): tsig key crashing sops-nix
    For whatever reason using the tsig key crashes sops-nix. Only use hickory as a recursor for now.

--- modules/hosts/zarqa/dns.nix
+++ modules/hosts/zarqa/dns.nix
@@ -10,11 +10,12 @@
         in
         {
           zone_signing_key = { inherit owner; };
-          tsig_secret_decoded = {
-            inherit owner;
-            format = "binary";
-            sopsFile = ./tsig_key.enc;
-          };
+          # FIXME: This isn't decrypted correctly -> Causes sops-nix to crash
+          # tsig_secret_decoded = {
+          #   inherit owner;
+          #   format = "binary";
+          #   sopsFile = ./tsig_key.enc;
+          # };
         };
 
       networking = {
@@ -58,7 +59,7 @@
           settings =
             let
               dnsLib = inputs.dns.lib;
-              inherit (dnsLib.combinators) letsEncrypt;
+              # inherit (dnsLib.combinators) letsEncrypt;
               dnsUtil = inputs.dns.util.${system};
             in
             {
@@ -138,46 +139,46 @@
                       opportunistic_encryption.enabled.persistence.path = "${nixos.config.services.hickory-dns.settings.directory}/opp_enc_state.toml";
                     };
                   }
-                  rec {
-                    zone = nixos.config.networking.domain;
-                    file = dnsUtil.writeZone zone rec {
-                      TTL = 60;
-                      SOA = {
-                        nameServer = builtins.head NS;
-                        adminEmail = nixos.config.invisible.email;
-                        serial = 2026012400;
-                      };
-                      NS =
-                        subdomains
-                        |> builtins.attrNames
-                        |> builtins.filter (lib.hasPrefix "ns")
-                        |> map (subdomain: "${subdomain}.${nixos.config.networking.domain}.");
-                      # TODO: These IP's aren't static. Use RFC2136 to update them dynamically
-                      A = [ "91.6.58.66" ];
-                      CAA = letsEncrypt SOA.adminEmail;
-                      subdomains = {
-                        "*" = { inherit A; };
-                        ns1 = { inherit A; };
-                        ns2 = { inherit A; };
-                      };
-                    };
-                    stores = {
-                      type = "sqlite";
-                      zone_path = file;
-                      journal_path = "${nixos.config.networking.domain}_dnssec_update.jrnl";
-                      allow_update = true;
-                      tsig_keys = lib.singleton {
-                        name = "tsig-key";
-                        algorithm = "hmac-sha256";
-                        # NOTE: 💢 Hickory requires the base64-DECODED secret.
-                        key_file = nixos.config.sops.secrets.tsig_secret_decoded.path;
-                      };
-                    };
-                    keys = lib.singleton {
-                      algorithm = "RSASHA256";
-                      key_path = nixos.config.sops.secrets.zone_signing_key.path;
-                    };
-                  }
+                  # rec {
+                  #   zone = nixos.config.networking.domain;
+                  #   file = dnsUtil.writeZone zone rec {
+                  #     TTL = 60;
+                  #     SOA = {
+                  #       nameServer = builtins.head NS;
+                  #       adminEmail = nixos.config.invisible.email;
+                  #       serial = 2026012400;
+                  #     };
+                  #     NS =
+                  #       subdomains
+                  #       |> builtins.attrNames
+                  #       |> builtins.filter (lib.hasPrefix "ns")
+                  #       |> map (subdomain: "${subdomain}.${nixos.config.networking.domain}.");
+                  #     # TODO: These IP's aren't static. Use RFC2136 to update them dynamically
+                  #     A = [ "91.6.58.66" ];
+                  #     CAA = letsEncrypt SOA.adminEmail;
+                  #     subdomains = {
+                  #       "*" = { inherit A; };
+                  #       ns1 = { inherit A; };
+                  #       ns2 = { inherit A; };
+                  #     };
+                  #   };
+                  #   stores = {
+                  #     type = "sqlite";
+                  #     zone_path = file;
+                  #     journal_path = "${nixos.config.networking.domain}_dnssec_update.jrnl";
+                  #     allow_update = true;
+                  #     tsig_keys = lib.singleton {
+                  #       name = "tsig-key";
+                  #       algorithm = "hmac-sha256";
+                  #       # NOTE: 💢 Hickory requires the base64-DECODED secret.
+                  #       key_file = nixos.config.sops.secrets.tsig_secret_decoded.path;
+                  #     };
+                  #   };
+                  #   keys = lib.singleton {
+                  #     algorithm = "RSASHA256";
+                  #     key_path = nixos.config.sops.secrets.zone_signing_key.path;
+                  #   };
+                  # }
                 ];
             };
         };
