e09b97520342a65069e902674ae6e19b74ed01a8
KP64 <karamalsadeh@hotmail.com>
2026-04-01 19:38:32 +0200

    chore(users/kg): disable zellij shell integration

--- modules/users/kg/dots/zellij/zellij.nix
+++ modules/users/kg/dots/zellij/zellij.nix
@@ -2,16 +2,16 @@
   flake.modules.homeManager.users-kg-zellij =
     { config, lib, ... }:
     {
+      # TODO: Reenable Integrations once Zellij has better (kitty) SSH support.
       programs = {
-        nushell.configFile.text = builtins.readFile ./start_zellij.nu;
+        # nushell.configFile.text = builtins.readFile ./start_zellij.nu;
         zellij = {
           enable = true;
           # Integrations are not enabled by default.
-          enableBashIntegration = true;
-          enableFishIntegration = true;
-          enableZshIntegration = true;
-
-          exitShellOnExit = true;
+          # enableBashIntegration = true;
+          # enableFishIntegration = true;
+          # enableZshIntegration = true;
+          # exitShellOnExit = true;
           settings = {
             default_shell = lib.mkIf config.programs.nushell.enable "nu";
             show_startup_tips = false;
