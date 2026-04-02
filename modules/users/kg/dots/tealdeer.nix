029501a5849f7e07de993bf16abc9961f85f3c77
KP64 <karamalsadeh@hotmail.com>
2026-04-02 18:03:09 +0200

    chore(users/kg): update tealdeer settings

--- modules/users/kg/dots/tealdeer.nix
+++ modules/users/kg/dots/tealdeer.nix
@@ -2,16 +2,10 @@
   flake.modules.homeManager.users-kg-tealdeer = {
     programs.tealdeer = {
       enable = true;
-      settings = {
-        display = {
-          compact = true;
-          show_title = true;
-        };
-        updates = {
-          auto_update = true;
-          auto_update_interval_hours = 24;
-          tls_backend = "rustls-with-native-roots";
-        };
+      settings.updates = {
+        auto_update = true;
+        auto_update_interval_hours = 24;
+        tls_backend = "rustls-with-native-roots";
       };
     };
   };
