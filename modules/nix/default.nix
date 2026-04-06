a49cca55fdd0621ecd86072de18be20e6e9ccce4
KP64 <karamalsadeh@hotmail.com>
2026-04-06 14:06:58 +0200

    chore(modules): nix lint todo

--- modules/nix/default.nix
+++ modules/nix/default.nix
@@ -15,6 +15,7 @@ let
     substituters = [ "https://nix-community.cachix.org" ];
     trusted-public-keys = [ "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" ];
 
+    # TODO: Enable these when the ecosystem is ready
     # lint-short-path-literals = "fatal";
     # lint-absolute-path-literals = "fatal";
     lint-url-literals = "fatal";
