f89525b75f124ff2e87397e68b2f0ed992081539
KP64 <karamalsadeh@hotmail.com>
2026-04-07 14:55:26 +0200

    refactor(hosts): move redlib from mahdi to zarqa

---
+++ modules/hosts/zarqa/services/redlib.nix
@@ -0,0 +1,51 @@
+{
+  flake.modules.nixos.hosts-zarqa =
+    { config, lib, ... }:
+    let
+      domain = "redlib.${config.networking.domain}";
+      inherit (config.lib.securityHeader) mkPP;
+    in
+    {
+      services = {
+        caddy.virtualHosts.${domain} = lib.mkIf config.services.redlib.enable {
+          extraConfig = # caddy
+            ''
+              reverse_proxy http://[::1]:${toString config.services.redlib.port}
+              header {
+                  Permissions-Policy "${
+                    mkPP {
+                      camera = "()";
+                      microphone = "()";
+                      geolocation = "()";
+                      usb = "()";
+                      bluetooth = "()";
+                      payment = "()";
+                      accelerometer = "()";
+                      gyroscope = "()";
+                      magnetometer = "()";
+                      midi = "()";
+                      serial = "()";
+                      hid = "()";
+                    }
+                  }"
+              }
+            '';
+        };
+
+        redlib = {
+          enable = true;
+          address = "[::1]";
+          port = 41297;
+          settings = {
+            REDLIB_ROBOTS_DISABLE_INDEXING = true;
+            REDLIB_ENABLE_RSS = true;
+            REDLIB_FULL_URL = "https://${domain}";
+
+            REDLIB_DEFAULT_BLUR_SPOILER = true;
+            REDLIB_DEFAULT_SHOW_NSFW = true;
+            REDLIB_DEFAULT_BLUR_NSFW = true;
+          };
+        };
+      };
+    };
+}
