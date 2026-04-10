0d73c0f6f61eb099452edbe7d4a0a56324055c20
KP64 <karamalsadeh@hotmail.com>
2026-04-10 18:07:10 +0200

    chore(pkgs): update hickory

--- pkgs/hickory-dns.nix
+++ pkgs/hickory-dns.nix
@@ -7,18 +7,18 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "hickory-dns";
-  version = "unstable-2026-04-04";
+  version = "unstable-2026-04-09";
 
   src = fetchFromGitHub {
     owner = "hickory-dns";
     repo = "hickory-dns";
-    rev = "fca38e4bed91e0c8688982479115be474ab2a929";
-    hash = "sha256-qLd8SNakSmIrNNqrzPBOIBCFKQl8hBLpitbbmFjkx/w=";
+    rev = "6e268f65c7eabedac0cdc3c1e4998fc8cd5f77ca";
+    hash = "sha256-rMdZ5MXDKM/IrqfIwqISROmqCj5MdPWkgSTKVz10KEk=";
   };
 
   __structuredAttrs = true;
 
-  cargoHash = "sha256-lTL1sXvHicVVd+79WPty7t+dq0SD67bJ2PG4sZsB3Lo=";
+  cargoHash = "sha256-mlAxCEDVl+cc3YAjwAT1wREbD/wbunn6b72MTqs1KRo=";
 
   buildFeatures = [
     "tls-ring" # DoT
@@ -35,6 +35,7 @@ rustPlatform.buildRustPackage (finalAttrs: {
     "mdns"
 
     "system-config"
+    "systemd"
     "rustls-platform-verifier"
     "resolver"
 
