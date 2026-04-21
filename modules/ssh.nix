9bbbdfe497a71fe1a735e54c632f84301af65d75
KP64 <karamalsadeh@hotmail.com>
2026-04-21 18:44:10 +0200

    fix(modules): ssh obsolete option

--- modules/ssh.nix
+++ modules/ssh.nix
@@ -1,36 +1,43 @@
 {
   flake.modules = {
-    nixos.ssh = {
-      services.openssh = {
-        enable = true;
-        startWhenNeeded = true;
-        # Only trust users.users.<name>.openssh.authorizedKeys.*
-        authorizedKeysInHomedir = false;
-        banner = ''
-          Unauthorized connection is disallowed.
-          This connection may be monitored.
-          Even then I can't stop you since you've come so far ¯\_(ツ)_/¯.
-          Although you are most likely an adversary I can only
-          ask you to be a bro and disclose how you got in after
-          you've finished with your shenanigans X).
-        '';
-        settings = {
-          PermitRootLogin = "no";
-          PasswordAuthentication = false;
-          KbdInteractiveAuthentication = false;
-        };
-      };
-
-      # TODO: Temporary. Remove once confident in the Security aspect.
-      services.fail2ban = {
-        enable = true;
-        ignoreIP = [ "192.168.2.0/24" ];
-        bantime-increment = {
+    nixos.ssh =
+      { pkgs, ... }:
+      {
+        services.openssh = {
           enable = true;
-          rndtime = "10m";
+          startWhenNeeded = true;
+          # Only trust users.users.<name>.openssh.authorizedKeys.*
+          authorizedKeysInHomedir = false;
+          settings = {
+            PermitRootLogin = "no";
+            PasswordAuthentication = false;
+            KbdInteractiveAuthentication = false;
+            Banner = toString (
+              pkgs.writeTextFile {
+                name = "ssh_banner";
+                text = ''
+                  Unauthorized connection is disallowed.
+                  This connection may be monitored.
+                  Even then I can't stop you since you've come so far ¯\_(ツ)_/¯.
+                  Although you are most likely an adversary I can only
+                  ask you to be a bro and disclose how you got in after
+                  you've finished with your shenanigans X).
+                '';
+              }
+            );
+          };
+        };
+
+        # TODO: Temporary. Remove once confident in the Security aspect.
+        services.fail2ban = {
+          enable = true;
+          ignoreIP = [ "192.168.2.0/24" ];
+          bantime-increment = {
+            enable = true;
+            rndtime = "10m";
+          };
         };
       };
-    };
 
     homeManager.ssh =
       {
