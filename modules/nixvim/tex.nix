a11a18bb26d22fda57bd72210db870aa545e80d0
KP64 <karamalsadeh@hotmail.com>
2026-04-25 10:41:15 +0200

    chore(neovim): remove preinstalled vimtex packages

--- modules/nixvim/tex.nix
+++ modules/nixvim/tex.nix
@@ -5,8 +5,16 @@
       extraPackagesAfter = [ pkgs.biber ];
 
       plugins = {
+        # Vimtex handles highlighting
+        treesitter.highlight.disable = [ "latex" ];
+
         texpresso.enable = true;
-        vimtex.enable = true;
+        vimtex = {
+          enable = true;
+          # Packages should be collected from the
+          # nix devShell of the project to compile
+          texlivePackage = null;
+        };
       };
     };
 }
