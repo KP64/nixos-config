8bd62fac00fcc857ca4c52e6d880b5e2b4754d8a
KP64 <karamalsadeh@hotmail.com>
2026-05-05 16:18:34 +0200

    fix(neovim): gh login error
    Do not enable unnecessary dependencies when not needed.

--- modules/nixvim/lsp/blink/git.nix
+++ modules/nixvim/lsp/blink/git.nix
@@ -1,26 +1,15 @@
 {
-  flake.modules.nixvim.blink-git =
-    { pkgs, ... }:
-    {
-      extraPackagesAfter = [ pkgs.glab ];
-      dependencies.gh = {
-        enable = true;
-        packageFallback = true;
-      };
-      plugins = {
-        blink-cmp-git.enable = true;
-        blink-cmp.settings.sources = {
-          default = [ "git" ];
-          providers.git = {
-            module = "blink-cmp-git";
-            name = "Git";
-            score_offset = 100;
-            opts = {
-              commit = { };
-              git_centers.github = { };
-            };
-          };
+  flake.modules.nixvim.blink-git = {
+    plugins = {
+      blink-cmp-git.enable = true;
+      blink-cmp.settings.sources = {
+        default = [ "git" ];
+        providers.git = {
+          module = "blink-cmp-git";
+          name = "Git";
+          score_offset = 100;
         };
       };
     };
+  };
 }
