a45567d985b6c13cd94e3528c635119ba2916dc3
KP64 <karamalsadeh@hotmail.com>
2026-05-17 16:30:11 +0200

    fix(hosts): add missing boot aspects

---
+++ modules/boot/secure-boot.nix
@@ -0,0 +1,41 @@
+{ den, inputs, ... }:
+{
+  flake-file.inputs.lanzaboote = {
+    type = "github";
+    owner = "nix-community";
+    repo = "lanzaboote";
+    inputs = {
+      nixpkgs.follows = "nixpkgs";
+      pre-commit.follows = "";
+    };
+  };
+
+  # TODO: Research measured boot
+  den.aspects.boot._.secure = {
+    includes = [ den.aspects.boot._.efi ];
+    nixos =
+      { lib, pkgs, ... }:
+      {
+        imports = [ inputs.lanzaboote.nixosModules.default ];
+
+        environment.systemPackages = [ pkgs.sbctl ];
+
+        boot = {
+          # Lanzaboote currently replaces the systemd-boot module.
+          # This setting is usually set to true in configuration.nix
+          # generated at installation time.
+          # So we force it to false for now.
+          loader.systemd-boot.enable = lib.mkForce false;
+          lanzaboote = {
+            enable = true;
+            pkiBundle = "/var/lib/sbctl";
+            autoGenerateKeys.enable = true;
+            autoEnrollKeys = {
+              enable = true;
+              autoReboot = true;
+            };
+          };
+        };
+      };
+  };
+}
