78014b8567a10e37b96d24d573b3504df7732b54
KP64 <karamalsadeh@hotmail.com>
2026-05-20 20:21:33 +0200

    chore(hosts): do not use router dns
    The router is pretty bad. For now just use Quad9 & Cloudflare.

--- modules/hosts/mahdi/networking.nix
+++ modules/hosts/mahdi/networking.nix
@@ -44,17 +44,26 @@ toplevel@{ den, ... }:
               "${config.staticIPv6}/64"
             ];
             gateway = [ "192.168.2.1" ];
-            dns = [ "192.168.2.1" ];
+            dns =
+              map (qdns: "${qdns}#dns.quad9.net") [
+                "9.9.9.9"
+                "149.112.112.112"
+                "2620:fe::fe"
+                "2620:fe::9"
+              ]
+              ++ map (cdns: "${cdns}#cloudflare-dns.com") [
+                "1.1.1.1"
+                "1.0.0.1"
+                "2606:4700:4700::1111"
+                "2606:4700:4700::1001"
+              ];
             networkConfig =
               let
                 inherit (lib) boolToYesNo;
               in
               {
-                DNSSEC = "allow-downgrade";
-                DNSOverTLS = "opportunistic";
-                # TODO: Reenforce when I figure Hickory DNS out.
-                # DNSSEC = boolToYesNo true;
-                # DNSOverTLS = boolToYesNo true;
+                DNSSEC = boolToYesNo true;
+                DNSOverTLS = boolToYesNo true;
                 MulticastDNS = boolToYesNo true;
                 LLMNR = boolToYesNo false;
               };
