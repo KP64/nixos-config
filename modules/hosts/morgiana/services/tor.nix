5572cd95af57cb427807b21664632b8733c74302
KP64 <karamalsadeh@hotmail.com>
2026-07-03 21:21:18 +0200

    style(host/morgiana): fmt

--- modules/hosts/morgiana/services/tor.nix
+++ modules/hosts/morgiana/services/tor.nix
@@ -17,41 +17,39 @@
         };
     };
 
-    nixos =
-      { config, ... }:
-      {
-        services.tor = {
+    nixos = { config, ... }: {
+      services.tor = {
+        enable = true;
+        openFirewall = true;
+        controlSocket.enable = true;
+        settings = {
+          # NOTE: This and the "tor" group are needed for the control Socket
+          CookieAuthFileGroupReadable = true;
+          DataDirectoryGroupReadable = true;
+
+          # NOTE: IPv4 address or FQDN that resolves to one
+          Address = config.networking.domain;
+
+          Nickname = "Torrifics";
+          # Specifically created for this.
+          ContactInfo = "torrifics@proton.me";
+
+          SocksPort = 0;
+          ORPort = [ 9001 ];
+          CookieAuthentication = true;
+
+          ProtocolWarnings = true;
+          HardwareAccel = 1;
+
+          # I hate my country's ISP internet plans...
+          RelayBandwidthRate = "3 MB";
+          RelayBandwidthBurst = "4 MB";
+        };
+        relay = {
           enable = true;
-          openFirewall = true;
-          controlSocket.enable = true;
-          settings = {
-            # NOTE: This and the "tor" group are needed for the control Socket
-            CookieAuthFileGroupReadable = true;
-            DataDirectoryGroupReadable = true;
-
-            # NOTE: IPv4 address or FQDN that resolves to one
-            Address = config.networking.domain;
-
-            Nickname = "Torrifics";
-            # Specifically created for this.
-            ContactInfo = "torrifics@proton.me";
-
-            SocksPort = 0;
-            ORPort = [ 9001 ];
-            CookieAuthentication = true;
-
-            ProtocolWarnings = true;
-            HardwareAccel = 1;
-
-            # I hate my country's ISP internet plans...
-            RelayBandwidthRate = "3 MB";
-            RelayBandwidthBurst = "4 MB";
-          };
-          relay = {
-            enable = true;
-            role = "relay";
-          };
+          role = "relay";
         };
       };
+    };
   };
 }
