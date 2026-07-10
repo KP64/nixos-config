8f2222a5238d7c5382d9b079ec451c00af76159f
KP64 <karamalsadeh@hotmail.com>
2026-07-10 13:16:05 +0200

    feat(lib): add topology

---
+++ modules/lib/topology.nix
@@ -0,0 +1,18 @@
+{ self, moduleWithSystem, ... }:
+{
+  den.aspects.customLib.nixos = moduleWithSystem (
+    { system, ... }: { lib, ... }: {
+      nix-lib.lib.topology.getHomeCidr = {
+        type = with lib.types; functionTo nonEmptyStr;
+        fn =
+          self.topology.${system}.config.networks.home.cidrv6
+          |> lib.splitString "/"
+          |> builtins.head
+          |> lib.removeSuffix "::";
+        description = ''
+          Returns the IPv6 Network Prefix of the home Network.
+        '';
+      };
+    }
+  );
+}
