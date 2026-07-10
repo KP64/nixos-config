264621d66ee5c2a8a069c43ca19dca0a0f3297ec
KP64 <karamalsadeh@hotmail.com>
2026-07-10 15:05:24 +0200

    feat(module/files): add wallpaper readme

---
+++ modules/files/wallpaper-readme.nix
@@ -0,0 +1,12 @@
+{
+  perSystem.files.file."assets/wallpapers/README.md".text = ''
+    # Wallpapers
+
+    ## Amazing Sources for wallpapers
+
+    1. [pixiv](https://pixiv.net)
+    2. [wallhaven](https://wallhaven.cc)
+    3. [dharmx](https://github.com/dharmx/walls)
+    4. [unixporn discord server](https://discord.gg/unixporn)
+  '';
+}
