3adfec2b4e1f4b75f904d86cb37089b314c70595
KP64 <karamalsadeh@hotmail.com>
2026-07-30 19:45:09 +0200

    chore: rely on selfhosted dns

--- modules/hosts/sheherazade/networking.nix
+++ modules/hosts/sheherazade/networking.nix
@@ -25,23 +25,9 @@ toplevel@{ den, ... }:
           networks."10-wlan0" = {
             name = "wlan0";
             DHCP = "ipv4";
-            dns =
-              map (qdns: "${qdns}#dns.quad9.net") [
-                "9.9.9.9"
-                "149.112.112.112"
-                "2620:fe::fe"
-                "2620:fe::9"
-              ]
-              ++ map (cdns: "${cdns}#one.one.one.one") [
-                "1.1.1.1"
-                "1.0.0.1"
-                "2606:4700:4700::1111"
-                "2606:4700:4700::1001"
-              ];
             networkConfig = {
               IPv6AcceptRA = true;
               IPv6PrivacyExtensions = false;
-              DNSOverTLS = true;
               DNSSEC = true;
               LLMNR = false;
               MulticastDNS = true;
--- modules/lib/topology.nix
+++ modules/lib/topology.nix
@@ -1,17 +1,30 @@
 { config, moduleWithSystem, ... }: {
   den.aspects.customLib.nixos = moduleWithSystem (
     { system, ... }: { lib, ... }: {
-      nix-lib.lib.topology.getHomeCidr6 = {
-        type = with lib.types; functionTo nonEmptyStr;
-        fn =
-          config.flake.topology.${system}.config.networks.home.cidrv6
-          |> lib.splitString "/"
-          |> builtins.head
-          |> lib.removeSuffix "::";
-        description = ''
-          Returns the IPv6 Network Prefix of the home Network.
-        '';
-      };
+      nix-lib.lib.topology =
+        let
+          inherit (config.flake.topology.${system}.config.networks) home;
+        in
+        {
+          getHomeCidr4 = {
+            type = with lib.types; functionTo nonEmptyStr;
+            fn =
+              home.cidrv4
+              |> lib.splitString "/"
+              |> builtins.head
+              |> lib.removeSuffix ".0";
+            description = "Returns the IPv4 Network Prefix of the home Network";
+          };
+          getHomeCidr6 = {
+            type = with lib.types; functionTo nonEmptyStr;
+            fn =
+              home.cidrv6
+              |> lib.splitString "/"
+              |> builtins.head
+              |> lib.removeSuffix "::";
+            description = "Returns the IPv6 Network Prefix of the home Network";
+          };
+        };
     }
   );
 }
