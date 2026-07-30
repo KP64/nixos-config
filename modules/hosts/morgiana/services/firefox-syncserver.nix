ce291dc5ad379c36ce9dd6df6f920a8d7228a231
KP64 <karamalsadeh@hotmail.com>
2026-07-30 22:44:45 +0200

    fix(host/morgiana): failing syncstorage

--- modules/hosts/morgiana/services/firefox-syncserver.nix
+++ modules/hosts/morgiana/services/firefox-syncserver.nix
@@ -33,6 +33,32 @@
       {
         services.firefox-syncserver = {
           enable = true;
+          # TODO: Remove this once https://github.com/NixOS/nixpkgs/issues/540669 is closed
+          package = pkgs.syncstorage-rs.overrideAttrs (
+            _:
+            let
+              swaggerSrc = pkgs.fetchurl {
+                url = "https://github.com/swagger-api/swagger-ui/archive/refs/tags/v5.17.14.zip";
+                hash = "sha256-SBJE0IEgl7Efuu73n3HZQrFxYX+cn5UU5jrL4T5xzNw=";
+              };
+
+              src = pkgs.fetchFromGitHub {
+                owner = "mozilla-services";
+                repo = "syncstorage-rs";
+                rev = "f084c3c78f91939a69ff10303f6579f7bf538beb";
+                hash = "sha256-d0rA/bWuo4gXvqI2inlvRI9NBP6ZRNSwLPkszNIkmhE=";
+              };
+            in
+            {
+              inherit src;
+              version = "0.23.3";
+              cargoDeps = pkgs.rustPlatform.fetchCargoVendor {
+                inherit src;
+                hash = "sha256-BJ5+6o57WlwsTerKCmOPXATPHQfjr5cRYMbqC8CIPg0=";
+              };
+              env.SWAGGER_UI_DOWNLOAD_URL = "file://${swaggerSrc}";
+            }
+          );
           secrets = config.sops.templates."firefox-syncserver.env".path;
           singleNode = {
             enable = true;
