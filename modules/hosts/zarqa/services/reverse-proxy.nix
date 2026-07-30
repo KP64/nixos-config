1add4df49ed34d770182086124ac5b2db8d9e4ca
KP64 <karamalsadeh@hotmail.com>
2026-07-30 21:38:52 +0200

    chore(host/zarqa): zero warning haproxy

--- modules/hosts/zarqa/services/reverse-proxy.nix
+++ modules/hosts/zarqa/services/reverse-proxy.nix
@@ -41,6 +41,7 @@ toplevel@{ den, ... }:
           ];
         })
         {
+          # TODO: Find a better way for HAProxy
           networking.firewall.allowedTCPPorts = [
             torORPort
             ntsPort
@@ -53,8 +54,7 @@ toplevel@{ den, ... }:
             config = ''
               global
                 ssl-mode-async
-                # TODO: Enable this
-                # zero-warning
+                zero-warning
 
                 harden.reject-privileged-ports.tcp on
                 harden.reject-privileged-ports.quic on
@@ -64,39 +64,56 @@ toplevel@{ den, ... }:
 
               defaults
                 mode tcp
-                option tcplog
-                option dontlognull
+
                 option clitcpka
                 option srvtcpka
 
+                timeout connect 5s
+                timeout client 1h
+                timeout server 1h
+
               frontend tor-in
                 bind [::]:${toString torORPort} v4v6
+                timeout client 24h
                 default_backend tor-out
               backend tor-out
+                timeout server 24h
+                timeout tunnel 24h
                 server tor [${morgiana.config.staticIPv6}]:${toString torORPort} check
 
               frontend nts-in
                 bind [::]:${toString ntsPort} v4v6
+                timeout client 30s
                 default_backend nts-out
               backend nts-out
+                timeout server 30s
                 server nts [${morgiana.config.staticIPv6}]:${toString ntsPort} check
 
               frontend forgejo-in
                 bind [::]:${toString forgejoSSHPort} v4v6
+                timeout client 5m
                 default_backend forgejo-out
               backend forgejo-out
+                timeout server 5m
+                timeout tunnel 5m
                 server forgejo [${mahdi.config.staticIPv6}]:${toString forgejoSSHPort} check
 
               frontend opengist-in
                 bind [::]:${toString OG_SSH_PORT} v4v6
+                timeout client 5m
                 default_backend opengist-out
               backend opengist-out
+                timeout server 5m
+                timeout tunnel 5m
                 server opengist [${mahdi.config.staticIPv6}]:${toString OG_SSH_PORT} check
 
               frontend minecraft-in
                 bind [::]:${toString minecraftPort} v4v6
+                timeout client 24h
                 default_backend minecraft-out
               backend minecraft-out
+                timeout server 24h
+                timeout tunnel 24h
                 server minecraft [${mahdi.config.staticIPv6}]:${toString minecraftPort} check
             '';
           };
