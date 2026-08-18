80a97d1d5f0b0229930ab7fe7315e089c46f6fd7
KP64 <karamalsadeh@hotmail.com>
2026-08-18 14:43:30 +0200

    fix(user/kg): nixvim build failure

--- modules/users/kg/dots/neovim/default.nix
+++ modules/users/kg/dots/neovim/default.nix
@@ -7,7 +7,6 @@ toplevel@{ inputs, ... }:
       enable = true;
       defaultEditor = true;
       vimdiffAlias = true;
-      nixpkgs.useGlobalPackages = true;
       imports = with toplevel.config.flake.modules.nixvim; [
         base
         lsp
