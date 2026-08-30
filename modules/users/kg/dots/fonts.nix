deaee37635310db6bbabf96fc8b837149682c5f3
KP64 <karamalsadeh@hotmail.com>
2026-08-30 19:14:55 +0200

    chore(user/kg): add default fonts

--- modules/users/kg/dots/fonts.nix
+++ modules/users/kg/dots/fonts.nix
@@ -1,23 +1,19 @@
 {
-  den.aspects.kg._.fonts = {
-    # TODO: Find out why this is enabled
-    nixos.fonts.enableDefaultPackages = false;
+  den.aspects.kg._.fonts.homeManager = { pkgs, ... }: {
+    fonts.fontconfig.enable = true;
 
-    homeManager = { pkgs, ... }: {
-      fonts.fontconfig.enable = true;
+    home.packages = with pkgs; [
+      nerd-fonts.jetbrains-mono
 
-      home.packages = with pkgs; [
-        nerd-fonts.jetbrains-mono
-
-        dejavu_fonts
-        freefont_ttf
-        gyre-fonts
-        liberation_ttf
-        unifont
-        noto-fonts-cjk-sans
-        noto-fonts-cjk-serif
-        noto-fonts-color-emoji
-      ];
-    };
+      # NOTE: Taken from https://github.com/NixOS/nixpkgs/blob/master/nixos/modules/config/fonts/packages.nix
+      dejavu_fonts
+      freefont_ttf
+      gyre-fonts
+      liberation_ttf
+      unifont
+      noto-fonts-cjk-sans
+      noto-fonts-cjk-serif
+      noto-fonts-color-emoji
+    ];
   };
 }
